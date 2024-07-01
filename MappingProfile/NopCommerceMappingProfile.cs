3b3e58378cc170a83c226e8e63bbe75f99d5377d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-01 20:42:37 +0200

    Add mapping profile for Automapper

---
+++ MappingProfile/NopCommerceMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models;
+
+namespace nopCommerceApi.MappingProfile
+{
+    public class NopCommerceMappingProfile : Profile
+    {
+        public NopCommerceMappingProfile()
+        {
+            CreateMap<Currency, CurrencyDto>();
+            CreateMap<StateProvince, StateProvinceDto>();
+            CreateMap<Language, LanguageDto>();
+            CreateMap<Country, CountryDto>();
+            CreateMap<Address, AddressDto>();
+            CreateMap<Customer, CustomerDto>();
+        }
+    }
+}
