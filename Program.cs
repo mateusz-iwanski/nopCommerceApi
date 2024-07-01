52f8acde6a28ce98f6762a09af944a0987f2d212
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-01 20:41:13 +0200

    Add NopCommerceContext and Automapper to builder service

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//builder.Services.AddDbContext<nopCommerceDbContext>();
+
+builder.Services.AddDbContext<NopCommerceContext>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
