1fa1d9cc48c54a0669c987a5b1e10b2189aef425
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-01 13:24:07 +0200

    Add Country DTO

---
+++ Models/CountryDto.cs
@@ -0,0 +1,17 @@
+﻿namespace nopCommerceApi.Models
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? TwoLetterIsoCode { get; set; }
+        public string? ThreeLetterIsoCode { get; set; }
+        public bool AllowsBilling { get; set; }
+        public bool AllowsShipping { get; set; }
+        public int NumericIsoCode { get; set; }
+        public bool SubjectToVat { get; set; }
+        public bool Published { get; set; }
+        public int DisplayOrder { get; set; }
+        public bool LimitedToStores { get; set; }
+    }
+}
