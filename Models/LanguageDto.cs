6f3b81390ad8df28cb748b43161ac47554c6bc98
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-01 13:36:04 +0200

    Add Language DTO

---
+++ Models/LanguageDto.cs
@@ -0,0 +1,16 @@
+﻿namespace nopCommerceApi.Models
+{
+    public class LanguageDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string LanguageCulture { get; set; } = null!;
+        public string? UniqueSeoCode { get; set; }
+        public string? FlagImageFileName { get; set; }
+        public bool Rtl { get; set; }
+        public bool LimitedToStores { get; set; }
+        public int DefaultCurrencyId { get; set; }
+        public bool Published { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
