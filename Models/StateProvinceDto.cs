7a63dfdfce4fd274e066143905c1d98486958b3a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-01 16:16:19 +0200

    Add StateProvince DTO

---
+++ Models/StateProvinceDto.cs
@@ -0,0 +1,14 @@
+﻿namespace nopCommerceApi.Models
+{
+    public class StateProvinceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Abbreviation { get; set; }
+        public int CountryId { get; set; }
+        public bool Published { get; set; }
+        public int DisplayOrder { get; set; }
+
+        public CountryDto Country { get; set; }
+    }
+}
