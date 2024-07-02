ffb2f1e08899cc8c86f8e047db4b39c034ce3848
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-02 14:28:15 +0200

    Grouping configuration, add Country configuration

---
+++ Entities/Configurations/CountryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class CountryConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.ToTable("Country");
+            builder.HasIndex(e => e.DisplayOrder, "IX_Country_DisplayOrder");
+            builder.Property(e => e.Name).HasMaxLength(100);
+            builder.Property(e => e.ThreeLetterIsoCode).HasMaxLength(3);
+            builder.Property(e => e.TwoLetterIsoCode).HasMaxLength(2);
+        }
+    }
+}
