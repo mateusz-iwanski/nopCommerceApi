16e0b92c43ee8518cdd160e43c1edbfd04032ca2
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-02 14:30:44 +0200

    Grouping configuration, add CustomerRole configuration

---
+++ Entities/Configurations/CustomerRoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class CustomerRoleConfiguration : IEntityTypeConfiguration<CustomerRole>
+    {
+        public void Configure(EntityTypeBuilder<CustomerRole> builder)
+        {
+            builder.ToTable("CustomerRole");
+
+            builder.Property(e => e.Name).HasMaxLength(255);
+            builder.Property(e => e.SystemName).HasMaxLength(255);
+        }
+    }
+}
