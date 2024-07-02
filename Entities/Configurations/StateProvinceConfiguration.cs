b4588be640e62337ab6e465ffb79a44bf3fef7ba
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-02 14:30:56 +0200

    Grouping configuration, add State configuration

---
+++ Entities/Configurations/StateProvinceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class StateProvinceConfiguration : IEntityTypeConfiguration<StateProvince>
+    {
+        public void Configure(EntityTypeBuilder<StateProvince> builder)
+        {
+            builder.ToTable("StateProvince");
+
+            builder.HasIndex(e => e.CountryId, "IX_StateProvince_CountryId");
+
+            builder.Property(e => e.Abbreviation).HasMaxLength(100);
+            builder.Property(e => e.Name).HasMaxLength(100);
+
+            builder.HasOne(d => d.Country).WithMany(p => p.StateProvinces)
+                .HasForeignKey(d => d.CountryId)
+                .HasConstraintName("FK_StateProvince_CountryId_Country_Id");
+        }
+    }
+}
