fe0df56539d23d0c7bdbebd04b14b004a7d2cf5f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-02 14:31:29 +0200

    Grouping configuration, add TirePrice configuration

---
+++ Entities/Configurations/TierPriceConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class TierPriceConfiguration : IEntityTypeConfiguration<TierPrice>
+    {
+        public void Configure(EntityTypeBuilder<TierPrice> builder)
+        {
+            builder.ToTable("TierPrice");
+
+            builder.HasIndex(e => e.CustomerRoleId, "IX_TierPrice_CustomerRoleId");
+
+            builder.HasIndex(e => e.ProductId, "IX_TierPrice_ProductId");
+
+            builder.Property(e => e.EndDateTimeUtc).HasPrecision(6);
+            builder.Property(e => e.Price).HasColumnType("decimal(18, 4)");
+            builder.Property(e => e.StartDateTimeUtc).HasPrecision(6);
+
+            builder.HasOne(d => d.CustomerRole).WithMany(p => p.TierPrices)
+                .HasForeignKey(d => d.CustomerRoleId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_TierPrice_CustomerRoleId_CustomerRole_Id");
+
+            builder.HasOne(d => d.Product).WithMany(p => p.TierPrices)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK_TierPrice_ProductId_Product_Id");
+        }
+    }
+}
