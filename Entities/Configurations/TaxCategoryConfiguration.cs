680952a51756721147f6a3b2a7db2b78eacf97ae
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-02 14:31:14 +0200

    Grouping configuration, add TaxCategory configuration

---
+++ Entities/Configurations/TaxCategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class TaxCategoryConfiguration : IEntityTypeConfiguration<TaxCategory>
+    {
+        public void Configure(EntityTypeBuilder<TaxCategory> builder)
+        {
+            builder.ToTable("TaxCategory");
+
+            builder.Property(e => e.Name).HasMaxLength(400);
+        }
+    }
+}
