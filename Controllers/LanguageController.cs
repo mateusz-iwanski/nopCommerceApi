1cc8645adbd3399be66b2c9f592d1f4be9c81838
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-02 15:29:47 +0200

    Add Language controller

---
+++ Controllers/LanguageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nopCommerceApi.Models;
+using nopCommerceApi.Services;
+
+namespace nopCommerceApi.Controllers
+{
+    public class LanguageController : ControllerBase
+    {
+        private readonly ILanguageService _languageService;
+
+        public LanguageController(ILanguageService languageService)
+        {
+            _languageService = languageService;
+        }
+
+        [HttpGet]
+        public ActionResult<LanguageDto> GetAll()
+        {
+            var languageDtos = _languageService.GetAll();
+            return Ok(languageDtos);
+        }
+    }
+}
