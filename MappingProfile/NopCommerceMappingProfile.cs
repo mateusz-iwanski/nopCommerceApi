5441d1123b9b555775a6ac054aabd946fbf105a9
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-06 12:55:37 +0200

    Customize automapper for trim all string values in Address Dto objects
    Customize only objects which are map from dto to entity

--- MappingProfile/NopCommerceMappingProfile.cs
+++ MappingProfile/NopCommerceMappingProfile.cs
@@ -19,8 +19,66 @@ namespace nopCommerceApi.MappingProfile
             CreateMap<Customer, CustomerDto>();
             CreateMap<AddressAttribute, AddressAttributeDto>();
 
-            CreateMap<CreatePolishEnterpriseAddressDto, Address>();
-            CreateMap<UpdatePolishEnterpriseAddressDto, Address>();
+
+            CreateMap<CreatePolishEnterpriseAddressDto, Address>()
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
+                .ForMember(x => x.Email, opt => opt.MapFrom(y => y.Email.Trim()))
+                .ForMember(x => x.Company, opt => opt.MapFrom(y => y.Company.Trim()))
+                .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Trim()))
+                .ForMember(x => x.City, opt => opt.MapFrom(y => y.City.Trim()))
+                .ForMember(x => x.Address1, opt => opt.MapFrom(y => y.Address1.Trim()))
+                .ForMember(x => x.Address2, opt => opt.MapFrom(y => y.Address2.Trim()))
+                .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
+                .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(y => y.PhoneNumber.Trim()));
+
+            CreateMap<UpdatePolishEnterpriseAddressDto, Address>()
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
+                .ForMember(x => x.Email, opt => opt.MapFrom(y => y.Email.Trim()))
+                .ForMember(x => x.Company, opt => opt.MapFrom(y => y.Company.Trim()))
+                .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Trim()))
+                .ForMember(x => x.City, opt => opt.MapFrom(y => y.City.Trim()))
+                .ForMember(x => x.Address1, opt => opt.MapFrom(y => y.Address1.Trim()))
+                .ForMember(x => x.Address2, opt => opt.MapFrom(y => y.Address2.Trim()))
+                .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
+                .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(y => y.PhoneNumber.Trim()));
+
+            CreateMap<CreatePolishUserAddressDto, Address>()
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
+                .ForMember(x => x.Email, opt => opt.MapFrom(y => y.Email.Trim()))
+                .ForMember(x => x.Company, opt => opt.MapFrom(y => y.Company.Trim()))
+                .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Trim()))
+                .ForMember(x => x.City, opt => opt.MapFrom(y => y.City.Trim()))
+                .ForMember(x => x.Address1, opt => opt.MapFrom(y => y.Address1.Trim()))
+                .ForMember(x => x.Address2, opt => opt.MapFrom(y => y.Address2.Trim()))
+                .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
+                .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(y => y.PhoneNumber.Trim()));
+
+            CreateMap<UpdateAddressDto, Address>()
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
+                .ForMember(x => x.Email, opt => opt.MapFrom(y => y.Email.Trim()))
+                .ForMember(x => x.Company, opt => opt.MapFrom(y => y.Company.Trim()))
+                .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Trim()))
+                .ForMember(x => x.City, opt => opt.MapFrom(y => y.City.Trim()))
+                .ForMember(x => x.Address1, opt => opt.MapFrom(y => y.Address1.Trim()))
+                .ForMember(x => x.Address2, opt => opt.MapFrom(y => y.Address2.Trim()))
+                .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
+                .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(y => y.PhoneNumber.Trim()));
+
+            CreateMap<CreateAddressDto, Address>()
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
+                .ForMember(x => x.Email, opt => opt.MapFrom(y => y.Email.Trim()))
+                .ForMember(x => x.Company, opt => opt.MapFrom(y => y.Company.Trim()))
+                .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Trim()))
+                .ForMember(x => x.City, opt => opt.MapFrom(y => y.City.Trim()))
+                .ForMember(x => x.Address1, opt => opt.MapFrom(y => y.Address1.Trim()))
+                .ForMember(x => x.Address2, opt => opt.MapFrom(y => y.Address2.Trim()))
+                .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
+                .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(y => y.PhoneNumber.Trim()));
 
         }
     }
