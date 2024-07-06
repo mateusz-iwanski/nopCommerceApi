f20a4bda9b162f9fb7879b49406ff8f5f2c32617
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-06 13:15:37 +0200

    Add CreatePolishUserAddressDto
    Always set Poland country for Polish users address

---
+++ Models/Address/CreatePolishUserAddressDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nopCommerceApi.Models.Address
+{
+    public class CreatePolishUserAddressDto : AddressDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
