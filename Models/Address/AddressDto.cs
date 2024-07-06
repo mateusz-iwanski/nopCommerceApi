f7aecb12beb68323e5372ed6ed109d3460f702e7
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-06 12:51:47 +0200

    Modify Address DTO, setter for email property trim value, set inheritance fo base address object, add function IsEnterpriseAddress which check if address is for enterprise user

--- Models/Address/AddressDto.cs
+++ Models/Address/AddressDto.cs
@@ -1,15 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using nopCommerceApi.Entities;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Xml;
+using nopCommerceApi.Entities;
+using Microsoft.Extensions.Primitives;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace nopCommerceApi.Models.Address
 {
-    public class AddressDto
+
+    public class AddressDto : BaseDto
     {
+        // If we not set/get Email over private _email, we will get an error 'Access violation' from IIS
+        private string _email;  
+
         public virtual string? FirstName { get; set; }
         public virtual string? LastName { get; set; }
         [EmailAddress]
-        public virtual string? Email { get; set; }
+        public virtual string Email
+        {
+            get => _email;
+            set => _email = value?.Trim();
+        }
+
         public virtual string? Company { get; set; }        
         public virtual string? County { get; set; }
         public virtual string? City { get; set; }
@@ -18,9 +33,8 @@ namespace nopCommerceApi.Models.Address
         public virtual string? ZipPostalCode { get; set; }
         public virtual string? PhoneNumber { get; set; }
         public virtual string? FaxNumber { get; set; }
-        public virtual string? CustomAttributes { get; set; }
-        public DateTime CreatedOnUtc { get; set; }
 
+        
         /// <summary>
         /// Get Value from CustomAttributes
         /// 
@@ -53,7 +67,38 @@ namespace nopCommerceApi.Models.Address
                     return default;
                 }
             }
+        }
 
+        public static bool IsEnterpriseAddress(nopCommerceApi.Entities.Address address, DbSet<AddressAttribute> addressAtributeDbSet)
+        {
+            string xmlString = address.CustomAttributes;
+
+            if (!string.IsNullOrEmpty(xmlString))
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+
+                xmlDoc.LoadXml(xmlString);
+
+                if (string.IsNullOrEmpty(xmlString))
+                    return false;
+
+                var addressAttribute = addressAtributeDbSet
+                    .FirstOrDefault(c => c.Name == "NIP" && c.AttributeControlTypeId == 4);
+
+                // Specify the ID you're looking for
+                int attributeId = addressAttribute.Id;
+                XmlNode addressAttributeNode = xmlDoc.SelectSingleNode($"/Attributes/AddressAttribute[@ID='{attributeId}']");
+
+                if (addressAttributeNode != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
         }
     }
 
