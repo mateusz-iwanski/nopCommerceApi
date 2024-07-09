737b0a6f4843695acd330f163d06afae16134def
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-09 14:31:51 +0200

    Add CreateWithNipPl_ValidData_ReturnsCreatedResult for test

--- Tests/AddressControllerTests.cs
+++ Tests/AddressControllerTests.cs
@@ -1,5 +1,13 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Specialized;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Identity.Client;
+using Moq;
+using nopCommerceApi.Controllers;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models.Address;
+using nopCommerceApi.Services;
 using System.Net;
 using Xunit;
 
@@ -8,11 +16,39 @@ namespace Tests
     public class AddressControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly HttpClient _client;
+        private readonly Mock<IAddressService> _addressServiceMock;
+        private readonly AddressController _controller;
 
         public AddressControllerTests(WebApplicationFactory<Program> factory)
         {
             // Arrange
             _client = factory.CreateClient();
+            _addressServiceMock = new Mock<IAddressService>();
+            _controller = new AddressController(_addressServiceMock.Object);
+        }
+
+        private IEnumerable<CreatePolishEnterpriseAddressDto> PolishEnterpriseAddressDto()
+        {
+            var dto = new List<CreatePolishEnterpriseAddressDto>
+            {
+                
+                new CreatePolishEnterpriseAddressDto()
+                { 
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "api@api.com",
+                    Company = "API Company",
+                    County = "małopolska",
+                    City = "Kraków",
+                    Address1 = "ul. API",
+                    Address2 = "API 2",
+                    ZipPostalCode = "30-000",
+                    PhoneNumber = "123456789",
+                    Nip = "7343216884"
+                },
+            };
+            
+            return dto;
         }
 
         [Fact]
@@ -27,7 +63,8 @@ namespace Tests
 
         [Theory]
         [InlineData("1")] // Assuming 1 is a valid ID and exists
-        [InlineData("")] // Empty string, shoud return GetAll data
+        [InlineData("")] // Empty string, shoud return GetAll data        [InlineData("")] // Empty string, shoud return GetAll data
+        [InlineData(null)] // Null, should return GetAll data
         public async Task GetById_WithParamater_ReturnsOkResult(string queryParams)
         {
             // Act
@@ -39,12 +76,12 @@ namespace Tests
 
         [Theory]
         [InlineData("999")] // Assuming 999 is a valid ID but does not exist
-        [InlineData("-1")] // Negative ID, likely invalid
+        [InlineData("-1")] // Negative ID, likely invalid if ID is expected to be positive
         [InlineData("0")] // Zero ID, might be considered invalid depending on the API
         [InlineData("abc")] // Non-numeric string, should be invalid if ID is expected to be numeric
         [InlineData("%20")] // URL encoded space, also invalid
-        [InlineData("2147483647")] // Max int value, boundary condition if ID is an int
-        [InlineData("1' OR '1'='1")] // SQL Injection attempt, should be handled gracefully
+        [InlineData("2147483648")] // Max int value + 1, boundary condition if ID is an int
+        [InlineData("1' OR '1'='1")] // SQL Injection attempt, should be handled gracefully        
         public async Task GetById_WithParamater_ReturnsNotFound(string queryParams)
         {
             // Act
@@ -52,6 +89,21 @@ namespace Tests
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Theory]
+        [JsonFileData("Data/AddressControllerData.json")]
+        public void CreateWithNipPl_ValidData_ReturnsCreatedResult(CreatePolishEnterpriseAddressDto addressDto)
+        {
+            _addressServiceMock.Setup(x => x.CreateWithNip(It.IsAny<CreatePolishEnterpriseAddressDto>()))
+                               .Returns(new Address { Id = 1 });
+
+            // Act
+            var result = _controller.CreateWithNipPl(addressDto);
+
+            // Assert
+            var createdResult = Assert.IsType<CreatedResult>(result);
+            createdResult.Location.Should().Be("/api/address/1");
         }
     }
 }
