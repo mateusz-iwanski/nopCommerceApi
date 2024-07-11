ff7b2782b393349a72fb37bb2e5aff537a323739
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-11 17:31:08 +0200

    Add Role controller
    It only has GetAll(). Cannot add/remove/update roles from the API because it is an enum type.

--- Models/User/Roles.cs
+++ Models/User/Roles.cs
@@ -5,7 +5,8 @@
     /// </summary>
     public enum Roles
     {
-        Admin,
-        User
+        Admin,  // Admin has full access to the API
+        User,  // User has limited access to the API
+        Viewer // Viewer has read-only access to the API
     }
 }
