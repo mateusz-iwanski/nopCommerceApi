f9bbde9352eed1a6c89bf24d2301cdddf2d3d80f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-11 17:36:59 +0200

    Add Role controller
    The role controller cannot perform add/remove/update actions because it is an enumerated type.

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using nopCommerceApi.Models.Address;
+using nopCommerceApi.Models.User;
+
+namespace nopCommerceApi.Controllers
+{
+    /// <summary>
+    /// Role controller is only responsible to get all roles
+    /// 
+    /// Role is an enum which contains all the roles that are available in the system.
+    /// We can't add/update/delete roles from the API. It's a fixed set of roles.
+    /// </summary>
+    [Route("api/account-role")]
+    [Authorize(Roles="Admin")]
+    public class RoleController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            // return all roles as list in table
+            var roles = Enum.GetValues(typeof(Roles))
+                           .Cast<Roles>()
+                           .Select(role => new
+                           {
+                               Name = role.ToString(),
+                               Value = (int)role
+                           });
+            return Ok(roles);
+        }
+    }
+}
