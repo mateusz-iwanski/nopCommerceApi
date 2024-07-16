84371371efdf101d0bd297a6bf498015f08c0747
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-16 10:27:05 +0200

    Set properly property for object

--- Models/TaxCategoryDto.cs
+++ Models/TaxCategoryDto.cs
@@ -2,21 +2,7 @@
 {
     public class TaxCategoryDto 
     {
-        public int? StateProvinceId { get; set; }
-        public int? CountryId { get; set; }
-
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? Company { get; set; }
-        public string? County { get; set; }
-        public string? City { get; set; }
-        public string? Address1 { get; set; }
-        public string? Address2 { get; set; }
-        public string? ZipPostalCode { get; set; }
-        public string? PhoneNumber { get; set; }
-        public string? FaxNumber { get; set; }
-        public string? CustomAttributes { get; set; }
-        public DateTime CreatedOnUtc { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
