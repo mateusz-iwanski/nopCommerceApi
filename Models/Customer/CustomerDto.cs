f2f352acf7fc213b281aa9e2b43293b568f02f13
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-16 10:23:39 +0200

    Change CustomerDto structure

--- Models/Customer/CustomerDto.cs
+++ Models/Customer/CustomerDto.cs
@@ -1,36 +1,53 @@
 ﻿using nopCommerceApi.Entities;
+using nopCommerceApi.Exceptions;
 using nopCommerceApi.Models.Address;
+using System.ComponentModel.DataAnnotations;
 
 namespace nopCommerceApi.Models.Customer
 {
-    public class CustomerDto
+    public class CustomerDto : BaseDto
     {
-        public int Id { get; set; }
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Company { get; set; }
-        public string? StreetAddress { get; set; }
-        public string? StreetAddress2 { get; set; }
-        public string? ZipPostalCode { get; set; }
-        public string? City { get; set; }
-        public string? County { get; set; }
-        public string? Phone { get; set; }
-        public string? VatNumber { get; set; }
-        public string? SystemName { get; set; }
-        public Guid CustomerGuid { get; set; }
-        public bool IsTaxExempt { get; set; }
-        public int VendorId { get; set; }
-        public bool Active { get; set; }
-        public bool Deleted { get; set; }
+        // If we not set/get Email over private _email, we will get an error 'Access violation' from IIS
+        protected string? _email { get; set; }
+        protected string? _username { get; set; }
 
-        public AddressDto? BillingAddress { get; set; }
-        public AddressDto? ShippingAddress { get; set; }
-        public LanguageDto? Language { get; set; }
+        // username can't be null, default it will be set as email when 
+        // not set username
+        public virtual string? Username
+        {
+            get => _username;
+            set => _username = value?.Trim(); //= !string.IsNullOrWhiteSpace(value) ? value.Trim() : throw new BadRequestException("Username is empty");
+        }
 
-        public CountryDto? Country { get; set; }
-        public StateProvinceDto? StateProvince { get; set; }
-        public CurrencyDto? Currency { get; set; }
+        [EmailAddress]
+        public virtual string Email
+        {
+            get => _email;
+            set => _email = value?.Trim();
+        }
+        public virtual string? FirstName { get; set; }
+        public virtual string? LastName { get; set; }
+        public virtual string? Company { get; set; }
+        public virtual string? StreetAddress { get; set; }
+        public virtual string? StreetAddress2 { get; set; }
+        public virtual string? ZipPostalCode { get; set; }
+        public virtual string? City { get; set; }
+        public virtual string? County { get; set; }
+        public virtual string? Phone { get; set; }
+        public virtual string? VatNumber { get; set; }
+        public virtual string? SystemName { get; set; }
+
+        public virtual AddressDto? BillingAddress { get; set; }
+        public virtual AddressDto? ShippingAddress { get; set; }
+        public virtual LanguageDto? Language { get; set; }
+        public virtual CountryDto? Country { get; set; }
+        public virtual StateProvinceDto? StateProvince { get; set; }
+        public virtual CurrencyDto? Currency { get; set; }
+
+        public virtual Guid CustomerGuid { get; set; }
+        public virtual bool IsTaxExempt { get; set; }
+        public virtual int VendorId { get; set; } 
+        public virtual bool Active { get; set; }
+        public virtual bool Deleted { get; set; }
     }
 }
