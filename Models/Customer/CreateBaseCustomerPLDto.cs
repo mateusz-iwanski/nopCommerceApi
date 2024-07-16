33b77130a6a67c1060a39b893added97a53979ab
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-16 10:26:05 +0200

    Add CreateBaseCustomerPLDto for creating Polish user by controller

---
+++ Models/Customer/CreateBaseCustomerPLDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nopCommerceApi.Models.Customer
+{
+    public class CreateBaseCustomerPLDto : CustomerDto
+    {
+        public override string Username
+        {
+            get => _username;
+            set => _username = value.Trim(); //= !string.IsNullOrWhiteSpace(value) ? value.Trim() : throw new BadRequestException("Username is empty");
+        }
+
+        [EmailAddress]
+        public override string Email
+        {
+            get => _email;
+            set => _email = value.Trim();
+        }
+
+        public override Guid CustomerGuid { get; set; } = Guid.NewGuid();
+        public override bool IsTaxExempt { get; set; } = false;
+        public override int VendorId { get; set; } = 0;
+        public override bool Active { get; set; } = true;
+        public override bool Deleted { get; set; } = false;
+    }
+}
