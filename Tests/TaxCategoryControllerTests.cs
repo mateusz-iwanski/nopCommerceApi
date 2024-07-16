47ba7de782dff3631578bdef5615b17c9847f2f4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-16 10:30:25 +0200

    Add test TaxCategoryControllerTests for tax category controller

---
+++ Tests/TaxCategoryControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Moq;
+using nopCommerceApi.Controllers.Customer;
+using nopCommerceApi.Services.Customer;
+
+namespace Tests
+{
+    public class TaxCategoryControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly Mock<ICustomerService> _customerServiceMock;
+        private readonly CustomerController _controller;
+
+        public TaxCategoryControllerTests(WebApplicationFactory<Program> factory)
+        {
+            // Arrange
+
+            // Register authorization and authentication for the service for web application
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // when a query requires authorization it will be delegated to FakePolicyEvaluator
+                    services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                });
+            })
+            .CreateClient();
+
+            _customerServiceMock = new Mock<ICustomerService>();
+            _controller = new CustomerController(_customerServiceMock.Object);
+        }
+
+
+    }
+}
