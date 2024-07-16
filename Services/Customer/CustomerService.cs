9ec97048cfc927c3ac993a784a82cac3a74e727a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-16 10:28:12 +0200

    Add AddBasePL function for CustomerService
    Create base customer data for Polish customers

--- Services/Customer/CustomerService.cs
+++ Services/Customer/CustomerService.cs
@@ -2,12 +2,14 @@
 using Microsoft.EntityFrameworkCore;
 using nopCommerceApi.Entities;
 using nopCommerceApi.Models.Customer;
+using System.Net;
 
 namespace nopCommerceApi.Services.Customer
 {
     public interface ICustomerService
     {
         IEnumerable<CustomerDto> GetAll();
+        Entities.Customer AddBasePL(CreateBaseCustomerPLDto createCustomerDto);
     }
 
     public class CustomerService : ICustomerService
@@ -20,7 +22,7 @@ namespace nopCommerceApi.Services.Customer
             _context = context;
             _mapper = mapper;
         }
-
+        
         public IEnumerable<CustomerDto> GetAll()
         {
             var customers = _context.Customers
@@ -35,5 +37,31 @@ namespace nopCommerceApi.Services.Customer
 
             return customerDtos;
         }
+
+        /// <summary>
+        /// Create with base customer data for Polish customers
+        /// </summary>
+        /// <param name="createCustomerDto"></param>
+        /// <returns></returns>
+        public Entities.Customer AddBasePL(CreateBaseCustomerPLDto createCustomerDto)
+        {
+            var customer = _mapper.Map<Entities.Customer>(createCustomerDto);
+            
+            var languageId = _context.Languages.FirstOrDefault(l => l.UniqueSeoCode == "pl").Id;
+            var currenyId = _context.Currencies.FirstOrDefault(c => c.CurrencyCode == "PLN").Id;
+            var countryId = _context.Countries.FirstOrDefault(c => c.TwoLetterIsoCode == "PL").Id;
+            // customer connect role with customer [Customer_CustomerRole_Mapping]
+            var cumerRoleId = _context.CustomerRoles.FirstOrDefault(cr => cr.SystemName == "Registered"); 
+
+            customer.LanguageId = languageId;
+            customer.CurrencyId = currenyId;
+            customer.CountryId = countryId;
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
     }
 }
