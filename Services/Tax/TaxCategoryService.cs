8e17f472dd63aa4c7b7d2ad95ac72e36ab1cdcaa
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-16 14:40:07 +0200

    Move creating default data for Polish tax category to TaxCategorySeeder

--- Services/Tax/TaxCategoryService.cs
+++ Services/Tax/TaxCategoryService.cs
@@ -8,17 +8,12 @@ namespace nopCommerceApi.Services
     {
         IEnumerable<TaxCategoryDto> GetAll();
         int GetLastDisplayOrder();
-        void AddDefaultTaxCategoryPL23();
-        void AddDefaultTaxCategoryPL8();
     }
 
     public class TaxCategoryService : ITaxCategoryService
     {
         private readonly NopCommerceContext _context;
         private readonly IMapper _mapper;
-
-        private readonly string _defaultTaxCategoryNamePl23 = "PL-23% (Podstawowa)";
-        private readonly string _defaultTaxCategoryNamePl8 = "PL-8% (Podstawowa)";
 
         public TaxCategoryService(NopCommerceContext context, IMapper mapper)
         {
@@ -47,48 +42,6 @@ namespace nopCommerceApi.Services
             }
             
             return lastTaxCategory.DisplayOrder;
-        }
-
-        /// <summary>
-        /// For Polish customers, add default tax category 23%
-        /// </summary>
-        public void AddDefaultTaxCategoryPL23()
-        {
-            var standardPl = _context.TaxCategories.FirstOrDefault(c => c.Name == _defaultTaxCategoryNamePl23);
-            if (standardPl == null)
-            {
-                var taxCategoryDto = new TaxCategoryDto
-                {
-                    Name = _defaultTaxCategoryNamePl23,                    
-                    DisplayOrder = GetLastDisplayOrder() == 0 ? 0 : GetLastDisplayOrder() + 1
-                };
-
-                var taxCategory = _mapper.Map<TaxCategory>(taxCategoryDto);
-
-                _context.TaxCategories.Add(taxCategory);
-                _context.SaveChanges();
-            }
-        }
-
-        /// <summary>
-        /// For Polish customers, add default tax category 8%
-        /// </summary>
-        public void AddDefaultTaxCategoryPL8()
-        {
-            var standardPl = _context.TaxCategories.FirstOrDefault(c => c.Name == _defaultTaxCategoryNamePl8);
-            if (standardPl == null)
-            {
-                var taxCategoryDto = new TaxCategoryDto
-                {
-                    Name = _defaultTaxCategoryNamePl8,
-                    DisplayOrder = GetLastDisplayOrder() == 0 ? 0 : GetLastDisplayOrder() + 1
-                };
-
-                var taxCategory = _mapper.Map<TaxCategory>(taxCategoryDto);
-
-                _context.TaxCategories.Add(taxCategory);
-                _context.SaveChanges();
-            }
         }
     }
 }
