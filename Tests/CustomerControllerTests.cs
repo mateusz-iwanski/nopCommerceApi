6848e1882af3e1923d4b551f3f31adbeb107e537
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-16 10:30:00 +0200

    Add test CustomerControllerTests for customer controller with test GetAll_WithoutParamater_ReturnsOkResult

---
+++ Tests/CustomerControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using FluentAssertions.Specialized;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Identity.Client;
+using Moq;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models.Address;
+using nopCommerceApi.Services;
+using System.Net;
+using Xunit;
+using FluentValidation;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Tests.Helpers;
+using nopCommerceApi.Controllers.Address;
+using nopCommerceApi.Services.Customer;
+using nopCommerceApi.Controllers.Customer;
+
+
+namespace Tests
+{
+    public class CustomerControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly Mock<ICustomerService> _customerServiceMock;
+        private readonly CustomerController _controller;
+
+        public CustomerControllerTests(WebApplicationFactory<Program> factory)
+        {
+            // Arrange
+
+            // Register authorization and authentication for the service for web application
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // when a query requires authorization it will be delegated to FakePolicyEvaluator
+                    services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                });
+            })
+            .CreateClient();
+
+            _customerServiceMock = new Mock<ICustomerService>();
+            _controller = new CustomerController(_customerServiceMock.Object);
+
+        }
+
+        [Fact]
+        public async Task GetAll_WithoutParamater_ReturnsOkResult()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/customer");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
