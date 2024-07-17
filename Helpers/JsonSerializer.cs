e9d82dbc9110c51c430a10e7b246f6b88072061e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-17 18:36:17 +0200

    Serialize an object to JSON while ignoring any circular references in the object for every IBaseDto object

---
+++ Helpers/JsonSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using nopCommerceApi.Models;
+
+
+namespace nopCommerceApi.Helpers
+{
+    public static class JsonSerializeHelper
+    {
+        /// <summary>
+        /// Serialize an object to JSON while ignoring any circular references in the object.
+        /// </summary>
+        public static string JsonSerializeReferenceLoopHandlingIgnore(this IBaseDto obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+        
+    }
+}
