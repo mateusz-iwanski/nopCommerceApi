e55717758f33519a4b1b07b96bfc5a06f602cf5f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-17 10:11:24 +0200

    Add a seeder to create a default administrator user when you start the application when it doesn't exist

---
+++ Seeder/AdminApiUserSeeder.cs
@@ -0,0 +1,35 @@
+﻿using nopCommerceApi.Entities;
+using nopCommerceApi.Models.User;
+using nopCommerceApi.Services.User;
+
+namespace nopCommerceApi.Seeder
+{
+    /// <summary>
+    /// Create a default administrator user when you start the application when it doesn't exist
+    /// 
+    /// It creates in users.json file
+    /// </summary>
+    public class AdminApiUserSeeder
+    {
+        private readonly NopCommerceContext _context;
+        private readonly IApiUserAccountService _apiUserAccountService;
+
+        public AdminApiUserSeeder(NopCommerceContext context, IApiUserAccountService apiUserAccountService)
+        {
+            _context = context;
+            _apiUserAccountService = apiUserAccountService;
+        }
+
+        public void Seed()
+        {
+            _apiUserAccountService.RegisterUser(
+                new RegisterApiUserDto
+                {
+                        Name = "Admin",
+                        Email = "admin@admin.com",
+                        PasswordHash = "admin",
+                        Role = "Admin"
+                });
+        }
+    }
+}
