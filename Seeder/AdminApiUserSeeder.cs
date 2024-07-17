b48fe08489a4c5679603172db45f1489364fa160
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-17 11:34:28 +0200

    Checks whether the user already exists, if so, it does nothing

--- Seeder/AdminApiUserSeeder.cs
+++ Seeder/AdminApiUserSeeder.cs
@@ -1,6 +1,8 @@
-﻿using nopCommerceApi.Entities;
+﻿using nopCommerceApi.Config;
+using nopCommerceApi.Entities;
 using nopCommerceApi.Models.User;
 using nopCommerceApi.Services.User;
+using System.Runtime;
 
 namespace nopCommerceApi.Seeder
 {
@@ -11,25 +13,46 @@ namespace nopCommerceApi.Seeder
     /// </summary>
     public class AdminApiUserSeeder
     {
+        private readonly string _adminName = "Admin";
+        private readonly string _adminEmail = "admin@admin.com";
+        private readonly string _adminPassword = "admin";
+        private readonly string _adminRole = "Admin";
+
+
         private readonly NopCommerceContext _context;
         private readonly IApiUserAccountService _apiUserAccountService;
+        private readonly IMySettings _settings;
 
-        public AdminApiUserSeeder(NopCommerceContext context, IApiUserAccountService apiUserAccountService)
+        public AdminApiUserSeeder(NopCommerceContext context, IApiUserAccountService apiUserAccountService, IMySettings mySettings)
         {
             _context = context;
             _apiUserAccountService = apiUserAccountService;
+            _settings = mySettings;
         }
 
-        public void Seed()
+        private void registerAdminUser()
         {
             _apiUserAccountService.RegisterUser(
                 new RegisterApiUserDto
                 {
-                        Name = "Admin",
-                        Email = "admin@admin.com",
-                        PasswordHash = "admin",
-                        Role = "Admin"
+                    Name = _adminName,
+                    Email = _adminEmail,
+                    PasswordHash = _adminPassword,
+                    Role = _adminRole
                 });
+        }
+
+        public void Seed()
+        {
+            var users = ApiUserService.GetUsersFromJson(_settings.UsersFilePath);
+
+            if (users != null)
+            {
+                if (!users.Any(u => u.Email == _adminEmail))
+                {
+                    registerAdminUser();
+                }
+            }
         }
     }
 }
