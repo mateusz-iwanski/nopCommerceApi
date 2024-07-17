18395833b4ace40a24abcea4711c982db2e7393b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-17 10:08:05 +0200

    Add summary to properties

--- Entities/CustomerRole.cs
+++ Entities/CustomerRole.cs
@@ -11,24 +11,55 @@ public partial class CustomerRole
 {
     public int Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the customer role name
+    /// </summary>
     public string Name { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets the customer role system name
+    /// </summary>
     public string? SystemName { get; set; }
 
+    /// <summary>
+    /// Check to allow customers in this role to get free shipping on their orders.
+    /// </summary>
     public bool FreeShipping { get; set; }
 
+    /// <summary>
+    /// Check to allow customers in this role to make tax free purchases.
+    /// </summary>
     public bool TaxExempt { get; set; }
 
+    /// <summary>
+    /// Check to make this role active.
+    /// </summary>
     public bool Active { get; set; }
 
+    /// <summary>
+    /// A value indicating whether it's a system role.
+    /// </summary>
     public bool IsSystemRole { get; set; }
 
+    /// <summary>
+    /// Check to force customers to change their passwords after a specified time.
+    /// </summary>
     public bool EnablePasswordLifetime { get; set; }
 
+    /// <summary>
+    /// Override default tax display type
+    /// </summary>
     public bool OverrideTaxDisplayType { get; set; }
 
+    /// <summary>
+    /// Default tax display type
+    /// </summary>
     public int DefaultTaxDisplayTypeId { get; set; }
 
+    /// <summary>
+    /// A customer is added to this customer role once a specified product is purchased (paid).
+    /// Please note that in case of refund or order cancellation you have to manually remove a customer from this role.
+    /// </summary>
     public int PurchasedWithProductId { get; set; }
 
     /// <summary>
