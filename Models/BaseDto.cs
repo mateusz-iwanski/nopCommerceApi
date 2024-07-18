b01ffa82a8c8687d582f8d7797d983368d67f6d3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-18 12:04:45 +0200

    Set on BaseDto UnexpectedProperties jsonignore for newtonsoft and System.Text.Json
    UnexpectedProperties will not visible in dto wehn serialize to json

--- Models/BaseDto.cs
+++ Models/BaseDto.cs
@@ -13,6 +13,8 @@ namespace nopCommerceApi.Models
         /// <c>UnexpectedProperties</c> is a dictionary that holds any properties that are not defined in the model.
         /// It is load when deserialize a JSON pauload into an object.
         /// </value>
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Newtonsoft.Json.JsonIgnore]
         [JsonExtensionData]
         public Dictionary<string, object> UnexpectedProperties { get; set; } = new Dictionary<string, object>();
     }
