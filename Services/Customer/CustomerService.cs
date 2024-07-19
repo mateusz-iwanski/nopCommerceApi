7be5dd41b5d17421fb71413b0e91725b0604fc8b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-19 13:20:40 +0200

    Add the client password to the service during creation

--- Services/Customer/CustomerService.cs
+++ Services/Customer/CustomerService.cs
@@ -72,12 +72,11 @@ namespace nopCommerceApi.Services.Customer
             _context.Customers.Add(customer);
             _context.SaveChanges();
 
-            string jsonString = createCustomerDto.JsonSerializeReferenceLoopHandlingIgnore();
-
-
-
-            _context.CustomerPasswords.Add(createPassword(PasswordFormat.Hashed, customer, createCustomerDto.Password));
+            // add password
+            _context.CustomerPasswords.Add(CustomerPasswordManager.CreatePassword(PasswordFormat.Hashed, customer, createCustomerDto.Password));
             _context.SaveChanges();
+
+            string jsonString = createCustomerDto.JsonSerializeReferenceLoopHandlingIgnore();
 
             return jsonString;
         }
@@ -106,143 +105,5 @@ namespace nopCommerceApi.Services.Customer
 
             return true;
         }
-
-        #region Password
-
-        public partial class SecureRandomNumberGenerator : RandomNumberGenerator
-        {
-            #region Field
-
-            protected bool _disposed;
-            protected readonly RandomNumberGenerator _rng;
-
-            #endregion
-
-            #region Ctor
-
-            public SecureRandomNumberGenerator()
-            {
-                _rng = Create();
-            }
-
-            #endregion
-
-            #region Methods
-
-            public int Next()
-            {
-                var data = new byte[sizeof(int)];
-                _rng.GetBytes(data);
-                return BitConverter.ToInt32(data, 0) & (int.MaxValue - 1);
-            }
-
-            public int Next(int maxValue)
-            {
-                return Next(0, maxValue);
-            }
-
-            public int Next(int minValue, int maxValue)
-            {
-                ArgumentOutOfRangeException.ThrowIfGreaterThan(minValue, maxValue);
-                return (int)Math.Floor(minValue + ((double)maxValue - minValue) * NextDouble());
-            }
-
-            public double NextDouble()
-            {
-                var data = new byte[sizeof(uint)];
-                _rng.GetBytes(data);
-                var randUint = BitConverter.ToUInt32(data, 0);
-                return randUint / (uint.MaxValue + 1.0);
-            }
-
-            public override void GetBytes(byte[] data)
-            {
-                _rng.GetBytes(data);
-            }
-
-            public override void GetNonZeroBytes(byte[] data)
-            {
-                _rng.GetNonZeroBytes(data);
-            }
-
-            /// <summary>
-            /// Dispose secure random
-            /// </summary>
-            public new void Dispose()
-            {
-                Dispose(true);
-                GC.SuppressFinalize(this);
-            }
-
-            // Protected implementation of Dispose pattern.
-            protected override void Dispose(bool disposing)
-            {
-                if (_disposed)
-                    return;
-
-                if (disposing)
-                {
-                    _rng?.Dispose();
-                }
-
-                _disposed = true;
-            }
-
-            #endregion
-        }
-
-        public static string GenerateRandomDigitCode(int length)
-        {
-            using var random = new SecureRandomNumberGenerator();
-            var str = string.Empty;
-            for (var i = 0; i < length; i++)
-                str = string.Concat(str, random.Next(10).ToString());
-            return str;
-        }
-
-        private CustomerPassword createPassword(PasswordFormat passwordFormat, Entities.Usable.Customer customer, string password) 
-        {
-            EncryptionService _encryptionService = new EncryptionService(
-                new SecuritySettings
-                {
-                    EncryptionKey = GenerateRandomDigitCode(16),
-                    AdminAreaAllowedIpAddresses = null,
-                    HoneypotEnabled = false,
-                    HoneypotInputName = "hpinput",
-                    AllowNonAsciiCharactersInHeaders = true,
-                    UseAesEncryptionAlgorithm = true,
-                    AllowStoreOwnerExportImportCustomersWithHashedPassword = true
-                });
-
-            var customerPassword = new CustomerPassword
-            {
-                CustomerId = customer.Id,
-                PasswordFormatId = (int)passwordFormat,
-                CreatedOnUtc = DateTime.UtcNow
-            };
-
-            switch (passwordFormat)
-            {
-                case PasswordFormat.Clear:
-                    customerPassword.Password = password;
-                    break;
-                case PasswordFormat.Encrypted:
-                    customerPassword.Password = _encryptionService.EncryptText(password);
-                    break;
-                case PasswordFormat.Hashed:
-                    // default from nopcommerce class NopCustomerServicesDefaults
-                    // Gets a password salt key size
-                    //public static int PasswordSaltKeySize => 5;
-                    //public static string DefaultHashedPasswordFormat => "SHA512"; - Gets or sets a customer password format (SHA1, MD5) when passwords are hashed (DO NOT edit in production environment)
-
-                    var saltKey = _encryptionService.CreateSaltKey(5);
-                    customerPassword.PasswordSalt = saltKey;
-                    customerPassword.Password = _encryptionService.CreatePasswordHash(password, saltKey, "SHA512");
-                    break;
-            }
-
-            return customerPassword;
-        }
-        #endregion
     }
 }
