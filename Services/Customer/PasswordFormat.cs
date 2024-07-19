1b86551715af81ce343be8468fb87cb793075809
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-19 13:32:10 +0200

    Default password format

---
+++ Services/Customer/PasswordFormat.cs
@@ -0,0 +1,23 @@
+﻿namespace nopCommerceApi.Services.Customer
+{
+    /// <summary>
+    /// Default password format
+    /// </summary>
+    public enum PasswordFormat
+    {
+        /// <summary>
+        /// Clear
+        /// </summary>
+        Clear = 0,
+
+        /// <summary>
+        /// Hashed
+        /// </summary>
+        Hashed = 1,
+
+        /// <summary>
+        /// Encrypted
+        /// </summary>
+        Encrypted = 2
+    }
+}
