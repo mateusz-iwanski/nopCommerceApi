b78a904b8806ce854de81a16cab51b7f5de4aa1e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-21 20:12:48 +0200

    Sperate Product Tag configurtation

---
+++ Entities/Configurations/ProductTagConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using nopCommerceApi.Entities.Usable;
+using System.Reflection.Emit;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class ProductTagConfiguration : IEntityTypeConfiguration<ProductTag>
+    {
+        public void Configure(EntityTypeBuilder<ProductTag> builder)
+        {
+            builder.ToTable("ProductTag");
+
+            builder.HasIndex(e => e.Name, "IX_ProductTag_Name");
+
+            builder.Property(e => e.Name).HasMaxLength(400);
+        }
+    }
+}
