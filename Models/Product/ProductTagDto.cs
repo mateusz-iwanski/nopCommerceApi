cc69c30b1a954a95b34e30a0fa7d9c5e7f467695
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-21 20:24:31 +0200

    Create product tag DTO

---
+++ Models/Product/ProductTagDto.cs
@@ -0,0 +1,8 @@
+﻿namespace nopCommerceApi.Models.Product
+{
+    public class ProductTagDto : BaseDto
+    {
+        public virtual string Name { get; set; }
+        public List<ProductDto> Products { get; set; }
+    }
+}
