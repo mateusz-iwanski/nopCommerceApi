d8100637423f569505afc2ce146b93ae20d25ecc
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-22 13:24:27 +0200

    Add validator for update Product Tag

---
+++ Validations/UpdateProductTagValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models.Product;
+
+namespace nopCommerceApi.Validations
+{
+    public class UpdateProductTagValidator : BaseValidator<UpdateProductTagDto>
+    {
+        public UpdateProductTagValidator(NopCommerceContext context)
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required")
+                .MaximumLength(400)
+                .WithMessage("Name must not exceed 400 characters");
+        }
+    }
+}
