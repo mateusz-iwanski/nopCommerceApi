f0e788bb536169eb49f3b1d35c99443bb3dacaab
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-23 09:37:02 +0200

    Create dto to read details from Tire Price

---
+++ Models/Product/TierPriceDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿using nopCommerceApi.Models.Customer;
+
+namespace nopCommerceApi.Models.Product
+{
+    public class TierPriceDetailsDto : TierPriceDto
+    {
+        public virtual CustomerRoleDto? CustomerRole { get; set; }
+        public virtual ProductDto Product { get; set;}
+    }
+}
