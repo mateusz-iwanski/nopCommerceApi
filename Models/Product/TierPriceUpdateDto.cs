67eea3211bcdaeacaf573eca027fb2aec8221418
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-23 09:37:48 +0200

    Create DTO for updating Tire Price

---
+++ Models/Product/TierPriceUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace nopCommerceApi.Models.Product
+{
+    public class TierPriceUpdateDto : BaseDto
+    {
+        public int? CustomerRoleId { get; set; }
+        public int ProductId { get; set; }
+        public int StoreId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? StartDateTimeUtc { get; set; }
+        public DateTime? EndDateTimeUtc { get; set; }
+    }
+}
