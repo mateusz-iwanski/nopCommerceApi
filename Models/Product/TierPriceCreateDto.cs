73159e16d895e27207fa9a5e76ba9cf083727744
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-23 09:35:41 +0200

    Create DTO for creatingTire Price

---
+++ Models/Product/TierPriceCreateDto.cs
@@ -0,0 +1,32 @@
+﻿
+namespace nopCommerceApi.Models.Product
+{
+    public class TierPriceCreateDto : BaseDto
+    {
+        /// <summary>
+        /// If null, applies to all customer roles.
+        /// </summary>
+        public int? CustomerRoleId { get; set; } = null;
+
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Leave empty if not multi-store. Default storeId is 0.
+        /// </summary>
+        public int? StoreId { get; set; } = 0;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Can be null. Represents the start date and time in UTC.
+        /// </summary>
+        public DateTime? StartDateTimeUtc { get; set; }
+
+        /// <summary>
+        /// Can be null. Represents the end date and time in UTC.
+        /// </summary>
+        public DateTime? EndDateTimeUtc { get; set; }
+    }
+}
