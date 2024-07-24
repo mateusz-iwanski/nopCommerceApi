d758b52751a3d14904843b6fdcfa0a548117b6e8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-24 20:49:23 +0200

    Create ProductAvailabilityRange DTO for reading

---
+++ Models/Product/ProductAvailabilityRangeDto.cs
@@ -0,0 +1,25 @@
+﻿namespace nopCommerceApi.Models.Product
+{
+    /// <summary>
+    /// Represents a product availability range
+    /// </summary>
+    /// <remarks>
+    /// Delivery dates are time ranges displaying approximate delivery time to a customer. 
+    /// The delivery dates can be applied to products and displayed on the product details pages.
+    /// Go to Configuration → Shipping → Dates and ranges.The two following panels will be displayed in the Dates and ranges window:
+    /// </remarks>
+    public class ProductAvailabilityRangeDto : BaseDto
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+    }
+}
