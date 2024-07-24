aeb7e6ed56996046f49ccc04aba936fb20413769
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-24 13:59:36 +0200

    Add controller for ProductTemaplate

---
+++ Controllers/Product/ProductTemplateController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nopCommerceApi.Services.Product;
+
+namespace nopCommerceApi.Controllers.Product
+{
+    [Route("api/product/template")]
+    [ApiController]
+    public class ProductTemplateController : ControllerBase
+    {
+        private readonly IProductTemplateService productTemplateService;
+
+        public ProductTemplateController(IProductTemplateService productTemplateService)
+        {
+            this.productTemplateService = productTemplateService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var productTemplates = productTemplateService.GetAll();
+            return Ok(productTemplates);
+        }
+    }
+}
