cdee66cdb5b2cbc0bdbb644e60f9e9e5320d4676
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-24 13:36:20 +0200

    Add ProductTemplate configuration for db context

---
+++ Entities/Configurations/ProductTemplateConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using nopCommerceApi.Entities.Usable;
+using System.Reflection.Emit;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class ProductTemplateConfiguration : IEntityTypeConfiguration<ProductTemplate>
+    {
+        public void Configure(EntityTypeBuilder<ProductTemplate> entity)
+        {
+            entity.ToTable("ProductTemplate");
+
+            entity.Property(e => e.Name).HasMaxLength(400);
+            entity.Property(e => e.ViewPath).HasMaxLength(400);
+        }
+    }
+}
