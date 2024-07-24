3bfe866c3b497a58164983dec040ce92db9669e5
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-24 13:38:10 +0200

    Make cleaunp in automapper mapping profile

--- MappingProfile/NopCommerceMappingProfile.cs
+++ MappingProfile/NopCommerceMappingProfile.cs
@@ -11,48 +11,62 @@ namespace nopCommerceApi.MappingProfile
     {
         public NopCommerceMappingProfile()
         {
-            CreateMap<CustomerRole, CustomerRoleDto>();            
+            #region Currency
+            // get
             CreateMap<Currency, CurrencyDto>();
-            CreateMap<StateProvince, StateProvinceDto>();
-            CreateMap<Language, LanguageDto>();
-            CreateMap<Country, CountryDto>();
-            
-            CreateMap<Customer, CustomerDto>();
-            CreateMap<CustomerRole, CustomerRoleDto>();
-            CreateMap<AddressAttribute, AddressAttributeDto>();
-            CreateMap<TaxCategory, TaxCategoryDto>();
-
-            #region Product
-
-            CreateMap<Product, ProductDto>();
-            CreateMap<ProductTag, ProductTagDto>();
-            CreateMap<ProductTag, ProductTagDetailsDto>();
-            
-            // create product
-            CreateMap<ProductTagCreateDto, ProductTag>();
-            
-            // tier price
-            CreateMap<TierPrice, TierPriceDto>();
-            CreateMap<TierPrice, TierPriceDetailsDto>();            
 
             #endregion
+
+            #region StateProvince
+            // get
+            CreateMap<StateProvince, StateProvinceDto>();
+
+            #endregion
+
+            #region Language
+            // get
+            CreateMap<Language, LanguageDto>();
+
+            #endregion
+
+            #region Country
+            // get
+            CreateMap<Country, CountryDto>();
+
+            #endregion
+
+            #region Customer
+            // get
+            CreateMap<Customer, CustomerDto>();
+            
+            // create
+            CreateMap<CustomerCreateBaseDto, Customer>()
+               .ForMember(x => x.Email, opt => opt.MapFrom(y => y.Email.Trim()))
+               .ForMember(x => x.Username, opt => opt.MapFrom(y => y.Username.Trim()))
+               .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
+               .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
+               .ForMember(x => x.Company, opt => opt.MapFrom(y => y.Company.Trim()))
+               .ForMember(x => x.StreetAddress, opt => opt.MapFrom(y => y.StreetAddress.Trim()))
+               .ForMember(x => x.StreetAddress2, opt => opt.MapFrom(y => y.StreetAddress2.Trim()))
+               .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
+               .ForMember(x => x.City, opt => opt.MapFrom(y => y.City.Trim()))
+               .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Trim()))
+               .ForMember(x => x.Phone, opt => opt.MapFrom(y => y.Phone.Trim()));
+
+            #endregion
+
+            #region CustomerRole
+            // get
+            CreateMap<CustomerRole, CustomerRoleDto>();
+
+            #endregion
+
+            #region Address
 
             CreateMap<Address, AddressDetailsDto>()
                 .ForMember(x => x.NIP, opt => opt.MapFrom(y => AddressDto.GetValueFromCustomAttribute(y.CustomAttributes)));
 
-            CreateMap<CustomerCreateBaseDto, Customer>()
-                .ForMember(x => x.Email, opt => opt.MapFrom(y => y.Email.Trim()))
-                .ForMember(x => x.Username, opt => opt.MapFrom(y => y.Username.Trim()))
-                .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
-                .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
-                .ForMember(x => x.Company, opt => opt.MapFrom(y => y.Company.Trim()))
-                .ForMember(x => x.StreetAddress, opt => opt.MapFrom(y => y.StreetAddress.Trim()))
-                .ForMember(x => x.StreetAddress2, opt => opt.MapFrom(y => y.StreetAddress2.Trim()))
-                .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
-                .ForMember(x => x.City, opt => opt.MapFrom(y => y.City.Trim()))
-                .ForMember(x => x.County, opt => opt.MapFrom(y => y.County.Trim()))
-                .ForMember(x => x.Phone, opt => opt.MapFrom(y => y.Phone.Trim()));
-
+            // create
             CreateMap<AddressCreatePolishEnterpriseDto, Address>()
                 .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
                 .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
@@ -112,6 +126,44 @@ namespace nopCommerceApi.MappingProfile
                 .ForMember(x => x.Address2, opt => opt.MapFrom(y => y.Address2.Trim()))
                 .ForMember(x => x.ZipPostalCode, opt => opt.MapFrom(y => y.ZipPostalCode.Trim()))
                 .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(y => y.PhoneNumber.Trim()));
+
+            #endregion
+
+            #region AddressAttribute
+            // get
+            CreateMap<AddressAttribute, AddressAttributeDto>();
+            #endregion
+
+            #region TaxCategory
+            //get
+            CreateMap<TaxCategory, TaxCategoryDto>();
+            #endregion
+
+            #region Product
+
+            CreateMap<Product, ProductDto>();
+            
+            // create product
+            CreateMap<ProductTagCreateDto, ProductTag>();
+
+            #endregion
+
+            #region ProductTag
+
+            CreateMap<ProductTag, ProductTagDto>();
+            CreateMap<ProductTag, ProductTagDetailsDto>();
+
+            #endregion
+
+            #region TrierPrice
+
+            CreateMap<TierPrice, TierPriceDto>();
+            CreateMap<TierPrice, TierPriceDetailsDto>();
+
+            #endregion
+
+
+            
 
         }
     }
