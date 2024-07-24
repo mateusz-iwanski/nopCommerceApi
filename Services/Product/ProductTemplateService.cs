f08a59b0750927c3f133e0b91b155c45e89c5408
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-24 14:07:10 +0200

    Create service for ProductTemplate

---
+++ Services/Product/ProductTemplateService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models.Product;
+
+namespace nopCommerceApi.Services.Product
+{
+    public class ProductTemplateService : BaseService
+    {
+        public ProductTemplateService(NopCommerceContext context, IMapper mapper, ILogger<ProductService> logger) : base(context, mapper, logger) { }
+
+        public IEnumerable<ProductTemplateDto> GetAll()
+        {
+            var productTemplates = _context.ProductTemplates.ToList();
+            var productTemplateDtos = _mapper.Map<List<ProductTemplateDto>>(productTemplates);
+
+            return productTemplateDtos;
+        }
+    }
+}
