f696a0914b42e386ed85e2b217f479cdbb233a7a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-25 10:38:41 +0200

    Create ProductAvailabilityRange configuration for db context

---
+++ Entities/Configurations/ProductAvailabilityRangeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using nopCommerceApi.Entities.Usable;
+using System.Reflection.Emit;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class ProductAvailabilityRangeConfiguration : IEntityTypeConfiguration<ProductAvailabilityRange>
+    {
+        public void Configure(EntityTypeBuilder<ProductAvailabilityRange> entity)
+        {
+            entity.ToTable("ProductAvailabilityRange");
+
+            entity.Property(e => e.Name).HasMaxLength(400);
+        }
+    }
+}
