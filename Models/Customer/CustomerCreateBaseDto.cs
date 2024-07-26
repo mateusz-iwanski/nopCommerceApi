c359b8df112fea2e36a4bc6b503d0210cb2df93f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-26 10:10:32 +0200

    Add summary and mark required fields in CustomerCreateBaseDto DTO

--- Models/Customer/CustomerCreateBaseDto.cs
+++ Models/Customer/CustomerCreateBaseDto.cs
@@ -3,7 +3,10 @@ using System.Text.Json.Serialization;
 
 namespace nopCommerceApi.Models.Customer
 {
-    public class CustomerCreateBaseDto : BaseDto //:CustomerDto
+    /// <summary>
+    /// <c>CustomerCreateBaseDto</c> uses for creating nopCommerce customer
+    /// </summary>
+    public class CustomerCreateBaseDto : BaseDto 
     {
         private bool _isTaxExempt = false; // every PL customer is not tax exempt
         private int _vendorId = 0; // disable multi-vendor option
@@ -11,23 +14,74 @@ namespace nopCommerceApi.Models.Customer
         private bool _deleted = false; // we create customer so is not deleted 
         private bool _isSystemAccount = false; // we create customer so is not system account
 
+        /// <summary>
+        /// Sets the username
+        /// </summary>
+        [Required]
         public string Username { get; set; }
-        public string? Password { get; set; }
 
+        /// <summary>
+        /// Sets the email
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Sets the email
+        /// </summary>
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Automatically sets the guid
+        /// </summary>
         [JsonIgnore]
         public virtual Guid CustomerGuid { get; set; } = Guid.NewGuid();
 
+        /// <summary>
+        /// Gets or sets the first name
+        /// </summary>
         public string? FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name
+        /// </summary>
         public string? LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the company name
+        /// </summary>
         public string? Company { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street address
+        /// </summary>
         public virtual string? StreetAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street address 2
+        /// </summary>
         public virtual string? StreetAddress2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the zip
+        /// </summary>
         public virtual string? ZipPostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the city
+        /// </summary>
         public virtual string? City { get; set; }
+
+        /// <summary>
+        /// Gets or sets the county
+        /// </summary>
         public virtual string? County { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number
+        /// </summary>
         public virtual string? Phone { get; set; }
 
         [JsonIgnore]
