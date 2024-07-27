d7b620365a6767626fe6a40a8266da2dbaf49a3e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-27 18:22:27 +0200

    Make password necessary in customer service

--- Services/Customer/CustomerService.cs
+++ Services/Customer/CustomerService.cs
@@ -75,13 +75,9 @@ namespace nopCommerceApi.Services.Customer
             _context.Customers.Add(customer);
             _context.SaveChanges();
 
-            // password is not necessary
-            if (!string.IsNullOrEmpty(createCustomerDto.Password))
-            {
-                // add password
-                _context.CustomerPasswords.Add(CustomerPasswordManager.CreatePassword(customer, createCustomerDto.Password, _settings));
-                _context.SaveChanges();
-            }
+            // add password
+            _context.CustomerPasswords.Add(CustomerPasswordManager.CreatePassword(customer, createCustomerDto.Password, _settings));
+            _context.SaveChanges();
 
             string jsonString = createCustomerDto.JsonSerializeReferenceLoopHandlingIgnore();
 
