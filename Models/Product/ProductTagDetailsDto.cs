1af87c56e2277960e56b092ee9e6fab20a21d3af
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-28 15:12:57 +0200

    Add summaries for product tag details

--- Models/Product/ProductTagDetailsDto.cs
+++ Models/Product/ProductTagDetailsDto.cs
@@ -1,7 +1,16 @@
 ﻿namespace nopCommerceApi.Models.Product
 {
+    /// <summary>
+    /// Product tag details Data Transfer Object
+    /// </summary>
+    /// <remarks>
+    /// Should be used only for the get method in controller
+    /// </remarks>
     public class ProductTagDetailsDto : ProductTagDto
     {
+        /// <summary>
+        /// List of products with this tag
+        /// </summary>
         public List<ProductDto> Products { get; set; }
     }
 }
