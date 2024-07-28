3e614d35348a796d9e8e321d78a0977c3ec626ca
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-28 15:13:11 +0200

    Add summaries for product tag create

--- Models/Product/ProductTagCreateDto.cs
+++ Models/Product/ProductTagCreateDto.cs
@@ -1,7 +1,13 @@
 ﻿namespace nopCommerceApi.Models.Product
 {
+    /// <summary>
+    /// Product tag create Data Transfer Object
+    /// </summary>
     public class ProductTagCreateDto : BaseDto
     {
+        /// <summary>
+        /// Sets the name
+        /// </summary>
         public virtual string Name { get; set; }
     }
 }
