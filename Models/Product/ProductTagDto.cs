953fb5ce54aa0baea17a5026b7840fd567219912
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-28 15:12:36 +0200

    Add summary for product tag dto

--- Models/Product/ProductTagDto.cs
+++ Models/Product/ProductTagDto.cs
@@ -1,5 +1,11 @@
 ﻿namespace nopCommerceApi.Models.Product
 {
+    /// <summary>
+    /// Product tag Data Transfer Object
+    /// </summary>
+    /// <remarks>
+    /// Should be used only for the get method in controller
+    /// </remarks>
     public class ProductTagDto : BaseDto
     {
         public virtual int Id { get; set; }
