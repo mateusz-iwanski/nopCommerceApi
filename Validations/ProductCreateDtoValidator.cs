91f5c2da3a992f8b06c4496bb576c1006b7bc260
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-28 15:01:00 +0200

    Make validations for ProductCreate DTO

--- Validations/ProductCreateDtoValidator.cs
+++ Validations/ProductCreateDtoValidator.cs
@@ -135,6 +135,42 @@ public class ProductCreateDtoValidator : AbstractValidator<ProductCreateDto>
             })
             .WithMessage("The rental price period does not exist.");
 
+        // ManagementInventory (enum) is required
+        // compare with appsettings.ini ManageInventoryMethodAvailableId
+        RuleFor(x => x.ManageInventoryMethodId)
+            .Must(manageInventoryMethodId =>
+            {
+                if (_settings.ManageInventoryMethodAvailableId.Split(",").Select(int.Parse)
+                                   .Contains(manageInventoryMethodId))
+                    return true;
+                return false;
+            })
+            .WithMessage("The manage inventory method does not exist.");
+
+        // LowStockActivity (enum) is required
+        // compare with appsettings.ini LowStockActivityAvailableId
+        RuleFor(x => x.LowStockActivityId)
+            .Must(lowStockActivityId =>
+            {
+                if (_settings.LowStockActivityAvailableId.Split(",").Select(int.Parse)
+                                                  .Contains(lowStockActivityId))
+                    return true;
+                return false;
+            })
+            .WithMessage("The low stock activity does not exist.");
+
+        // BackorderMode (enum) is required
+        // compare with appsettings.ini BackorderModeAvailableId
+        RuleFor(x => x.BackorderModeId)
+            .Must(backorderModeId =>
+            {
+                if (_settings.BackorderModeAvailableId.Split(",").Select(int.Parse)
+                                                     .Contains(backorderModeId))
+                    return true;
+                return false;
+            })
+            .WithMessage("The backorder mode does not exist.");
+
         // check that ProductTemplates exists with id
         RuleFor(x => x.ProductTemplateId)
             .Must((productTemplateId) =>
@@ -192,5 +228,25 @@ public class ProductCreateDtoValidator : AbstractValidator<ProductCreateDto>
                 return true;
             })
             .WithMessage("The delivery date does not exist.");
+
+        // check if ProductAvailabilityRangeId exists
+        RuleFor(x => x.ProductAvailabilityRangeId)
+            .Must((productAvailabilityRangeId) =>
+            {
+                if (productAvailabilityRangeId != 0)
+                    return _context.ProductAvailabilityRanges.Any(c => c.Id == productAvailabilityRangeId);
+                return true;
+            })
+            .WithMessage("The product availability range does not exist.");
+
+        // check if WarehouseId
+        RuleFor(x => x.WarehouseId)
+            .Must((warehouseId) =>
+            {
+                if (warehouseId != 0)
+                    return _context.Warehouses.Any(c => c.Id == warehouseId);
+                return true;
+            })
+            .WithMessage("The warehouse does not exist.");
     }
 }
