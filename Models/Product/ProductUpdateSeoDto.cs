c449e822c1ccce4ccf4766fd9031e1a1b954d208
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-30 21:31:09 +0200

    Create ProductUpdateSeoDto

---
+++ Models/Product/ProductUpdateSeoDto.cs
@@ -0,0 +1,23 @@
+﻿namespace nopCommerceApi.Models.Product
+{
+    public class ProductUpdateSeoDto : BaseDto
+    {
+        /// <summary>
+        /// ## MetaKeywords 
+        /// ### Set the meta keywords.
+        /// </summary>
+        public virtual string? MetaKeywords { get; set; }
+
+        /// <summary>
+        /// ## MetaTitle
+        /// ### Set the meta description.
+        /// </summary>
+        public virtual string? MetaTitle { get; set; }
+
+        /// <summary>
+        /// ## MetaDescription
+        /// ### Meta description to be added to product page header.
+        /// </summary>
+        public virtual string? MetaDescription { get; set; }
+    }
+}
