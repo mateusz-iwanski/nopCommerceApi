1b1dba1b2c4fd233c7a78ddb4aab05481f35e6a6
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-30 21:25:58 +0200

    Add action UpdateInformation

--- Controllers/Product/ProductController.cs
+++ Controllers/Product/ProductController.cs
@@ -50,26 +50,54 @@ namespace nopCommerceApi.Controllers.Product
         }
 
         /// <summary>
-        /// Create a new product with minimal information
+        /// Create a new product with minimal information and all rest fields set to default values
         /// </summary>
         /// <remarks>
         /// Only the required properties are included in this DTO, 
-        /// rest fields set up with default values.
+        /// rest fields are set up with default values.
+        /// Next you have to use the update method to add more information.
         /// </remarks>
-        [HttpPost("minimal")]
+        [HttpPost("add/minimal")]
         public IActionResult CreateMinimal([FromBody] ProductCreateMinimalDto productDto)
         {
             var product = _productService.CreateMinimal(productDto);
             return Created($"api/product/{ product.Id}", product);
         }
 
+        /// <summary>
+        /// Update product information by id
+        /// </summary>
+        /// <remarks>
+        /// Note: Look out on boolean and datetime fields, if you not include them in the request, 
+        /// boolean will set on false and datetime on default value (null).
+        /// </remarks>
+        [HttpPost("update/information/{id}")]
+        public IActionResult UpdateInformation(int id, [FromBody] ProductUpdateInformationDto productDto)
+        {
+            _productService.UpdateInformation(id, productDto);
+            return Ok($"Update product by id: {id}");
+        }
+
+
+        [HttpPost("update/seo/{id}")]
+        public IActionResult UpdateSeo(int id, [FromBody] ProductUpdateSeoDto productDto)
+        {
+            _productService.UpdateSeo(id, productDto);
+            return Ok($"Update product by id: {id}");
+        }
+
         // PUT: api/product/5
         /// <summary>
         /// Update product by id
         /// </summary>
+        /// <remarks>
+        /// If you don't want to update certain fields, don't add them to your requests.
+        /// If you do not add some of a fields to the request, this fields will not be updated.
+        /// </remarks>
         [HttpPut("{id}")]
-        public IActionResult Update(int id, [FromBody] string value)
+        public IActionResult Update(int id, [FromBody] ProductUpdateDto productDto)
         {
+            _productService.Update(id, productDto);
             return Ok($"Update product by id: {id}");
         }
 
