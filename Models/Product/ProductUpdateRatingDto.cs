7b64420cb2e5ba0983f69f97efd98b2cb75845c8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-07-31 13:03:48 +0200

    Create ProductUpdateRatingDto

---
+++ Models/Product/ProductUpdateRatingDto.cs
@@ -0,0 +1,22 @@
+﻿namespace nopCommerceApi.Models.Product
+{
+    public class ProductUpdateRatingDto : BaseDto
+    {
+        /// <summary>
+        /// ## ApprovedRatingSum
+        /// ### Set the rating sum (approved reviews).
+        /// #### Look on Configuration → Settings → Catalog settings for more details.
+        /// #### When required is set in new review type, customers have to choose an appropriate rating value before they can continue.
+        /// *Default = 0 (not approved)*
+        /// </summary>
+        public virtual int ApprovedRatingSum { get; set; }
+
+        /// <summary>
+        /// ## NotApprovedRatingSum
+        /// ### Set the rating sum (not approved reviews).
+        /// #### Look on Configuration → Settings → Catalog settings for more details.
+        /// *Default = 0 (not approved)*
+        /// </summary>
+        public virtual int NotApprovedRatingSum { get; set; }
+    }
+}
