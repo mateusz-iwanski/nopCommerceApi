65860710e774ce210dff61c18356ec0a1856e5b7
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-01 16:34:51 +0200

    Create service and controller for category

---
+++ Controllers/Category/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nopCommerceApi.Models.Category;
+using nopCommerceApi.Services.Category;
+
+namespace nopCommerceApi.Controllers.Category
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        /// <summary>
+        /// Get all categories
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<CategoryDto> GetAll()
+        {
+            var categoriesDto = _categoryService.GetAll();
+            return categoriesDto;
+        }
+
+        /// <summary>
+        /// Get category by id
+        /// </summary>
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var categoryDto = _categoryService.GetById(id);
+
+            return Ok(categoryDto);
+        }
+
+        /// <summary>
+        /// Create a new category
+        /// </summary>
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateCategoryDto categoryDto)
+        {
+            var category = _categoryService.Create(categoryDto);
+            return Created($"api/category/{ category.Id}", category);
+        }
+
+        /// <summary>
+        /// Update category 
+        /// </summary>
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] UpdateCategoryDto categoryDto)
+        {
+            var category = _categoryService.Update(id, categoryDto);
+            return Ok(category);
+        }
+
+        /// <summary>
+        /// Delete category
+        /// </summary>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _categoryService.Delete(id);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ Services/Category/CategoryService.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Exceptions;
+using nopCommerceApi.Models.Category;
+
+namespace nopCommerceApi.Services.Category
+{
+    public interface ICategoryService
+    {
+        CategoryDto Create(CreateCategoryDto createCategoryDto);
+        bool Delete(int id);
+        IEnumerable<CategoryDto> GetAll();
+        CategoryDto GetById(int id);
+        CategoryDto Update(int id, UpdateCategoryDto updateCategoryDto);
+    }
+
+    public class CategoryService : BaseService, ICategoryService
+    {
+        public CategoryService(NopCommerceContext context, IMapper mapper, ILogger<CategoryService> logger) : base(context, mapper, logger) { }
+
+        public IEnumerable<CategoryDto> GetAll()
+        {
+            var categories = _context.Categories.ToList();
+            var categoryDtos = _mapper.Map<List<CategoryDto>>(categories);
+
+            return categoryDtos;
+        }
+
+        public CategoryDto GetById(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null) throw new NotFoundExceptions($"Category with id {id} not found");
+
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+
+            return categoryDto;
+        }
+
+        public CategoryDto Create(CreateCategoryDto createCategoryDto)
+        {
+            var category = _mapper.Map<Entities.Usable.Category>(createCategoryDto);
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+
+            return categoryDto;
+        }
+
+        public CategoryDto Update(int id, UpdateCategoryDto updateCategoryDto)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null) throw new NotFoundExceptions($"Category with id {id} not found");
+
+            updateCategoryDto.Id = id;
+            updateCategoryDto.CreatedOnUtc = category.CreatedOnUtc;
+
+            _mapper.Map(updateCategoryDto, category);
+
+            _context.SaveChanges();
+
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+
+            return categoryDto;
+        }
+
+        public bool Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null) throw new NotFoundExceptions($"Category with id {id} not found");
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
