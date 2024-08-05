1f6009dab5dba6bb21a010be799596b939aedfeb
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-05 20:27:31 +0200

    Create DTO for ProductCategoryMapping

---
+++ Models/ProductCategory/ProductCategoryMappingDto.cs
@@ -0,0 +1,17 @@
+﻿namespace nopCommerceApi.Models.ProductCategory
+{
+    public class ProductCategoryMappingDto : BaseDto
+    {
+
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public bool IsFeaturedProduct { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+    }
+}
