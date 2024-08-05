6d14b5302a6fa91dcd0b519d2f6e67753e667ca9
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-05 21:41:01 +0200

    Create controller for ProductCategoryMapping

--- Controllers/Category/CategoryController.cs
+++ Controllers/Category/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using nopCommerceApi.Entities.Usable;
 using nopCommerceApi.Models.Category;
+using nopCommerceApi.Models.ProductCategory;
 using nopCommerceApi.Services.Category;
 
 namespace nopCommerceApi.Controllers.Category
@@ -9,10 +11,12 @@ namespace nopCommerceApi.Controllers.Category
     public class CategoryController : ControllerBase
     {
         public readonly ICategoryService _categoryService;
+        public readonly IProductCategoryMappingService _productCategoryMappingService;
 
-        public CategoryController(ICategoryService categoryService)
+        public CategoryController(ICategoryService categoryService, IProductCategoryMappingService productCategoryMappingService)
         {
             _categoryService = categoryService;
+            _productCategoryMappingService = productCategoryMappingService;
         }
 
         /// <summary>
@@ -66,5 +70,54 @@ namespace nopCommerceApi.Controllers.Category
             return Ok(result);
         }
 
+        /// <summary>
+        /// Get all cateogry with associated products
+        /// </summary>
+        [HttpGet("mapping")]    
+        public IEnumerable<ProductCategoryMappingDto> ProductCategoryMappingDtosGetAll()
+        {
+            var productCategoryMappingDtos = _productCategoryMappingService.GetAll();
+            return productCategoryMappingDtos;
+        }
+
+        /// <summary>
+        /// Associate a product with a category
+        /// </summary>
+        [HttpPost("mapping")]
+        public IActionResult ProductCategoryMappingDtosCreate([FromBody] ProductCategoryMappingCreateDto productCategoryMappingCreateDto)
+        {
+            var productCategoryMapping = _productCategoryMappingService.Create(productCategoryMappingCreateDto);
+            return Created($"api/category/mapping/{productCategoryMapping.Id}", productCategoryMapping);
+        }
+
+        /// <summary>
+        /// Delete association between a product and a category
+        /// </summary>
+        [HttpDelete("mapping/{productId}/{categoryId}")]
+        public IActionResult ProductCategoryMappingDtosDelete(int productId, int categoryId)
+        {
+            var result = _productCategoryMappingService.Delete(productId, categoryId);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get all product categories mappings associated with a product
+        /// </summary>
+        [HttpGet("mapping/product/{productId}")]
+        public IEnumerable<ProductCategoryMapping> getAllAssociateWithProduct(int productId)
+        {
+            var productCategoryMappings = _productCategoryMappingService.GetAllAssociateWithProduct(productId);
+            return productCategoryMappings;
+        }
+
+        /// <summary>
+        /// Get all product categories mappings associated with a category
+        /// </summary>
+        [HttpGet("mapping/category/{categoryId}")]
+        public IEnumerable<ProductCategoryMapping> GetProductCategoryMappings(int categoryId)
+        {
+            var productCategoryMappings = _productCategoryMappingService.GetAllAssociateWithCategory(categoryId);
+            return productCategoryMappings;
+        }   
     }
 }
