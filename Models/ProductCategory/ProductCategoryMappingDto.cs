d1779b3a0104b8c1b787e0bc363fe57e7ed6efea
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-05 21:38:11 +0200

    Create product category mapping DTOs

--- Models/ProductCategory/ProductCategoryMappingDto.cs
+++ Models/ProductCategory/ProductCategoryMappingDto.cs
@@ -1,17 +1,43 @@
-﻿namespace nopCommerceApi.Models.ProductCategory
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nopCommerceApi.Models.ProductCategory
 {
+    /// <summary>
+    /// Product category mapping DTO
+    /// </summary>
+    /// <remarks>
+    /// Associate a product with a category
+    /// </remarks>
     public class ProductCategoryMappingDto : BaseDto
     {
 
-        public int Id { get; set; }
+        public virtual int Id { get; set; }
 
-        public int CategoryId { get; set; }
+        /// <summary>
+        /// Gets or set the category identifier
+        /// </summary>
+        [Required]
+        public virtual int CategoryId { get; set; }
 
-        public int ProductId { get; set; }
+        /// <summary>
+        /// Gets or set the product identifier
+        /// </summary>
+        [Required]
+        public virtual int ProductId { get; set; }
 
-        public bool IsFeaturedProduct { get; set; }
+        /// <summary>
+        /// Gets or set a value indicating whether the product is featured
+        /// *Default = false*
+        /// </summary>
+        public virtual bool IsFeaturedProduct { get; set; }
 
-        public int DisplayOrder { get; set; }
+        /// <summary>
+        /// Gets or set the display order        
+        /// </summary>
+        /// <remarks>
+        /// *Default = false*
+        /// </remarks>
+        public virtual int DisplayOrder { get; set; }
 
     }
 }
---
+++ Models/ProductCategory/ProductCategoryMappingUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace nopCommerceApi.Models.ProductCategory
+{
+    public class ProductCategoryMappingUpdateDto : ProductCategoryMappingDto
+    {
+        [JsonIgnore]
+        public override int Id { get; set; }
+    }
+}
