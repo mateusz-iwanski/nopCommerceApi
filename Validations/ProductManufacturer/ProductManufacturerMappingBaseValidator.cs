5af4951945540967aa32c95637f3f3997e14d1b4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-06 15:02:44 +0200

    Create product manufacturer mapping validators

---
+++ Validations/ProductManufacturer/ProductManufacturerMappingBaseValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using nopCommerceApi.Config;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Entities.Usable;
+using nopCommerceApi.Models.ProductCategory;
+using nopCommerceApi.Models.ProductManufacturer;
+
+namespace nopCommerceApi.Validations.ProductManufacturer
+{
+    public class ProductManufacturerMappingBaseValidator<T> : BaseValidator<T> where T : ProductManufacturerMappingDto
+    {
+        protected readonly NopCommerceContext _context;
+
+        public ProductManufacturerMappingBaseValidator(NopCommerceContext context)
+        {
+            _context = context;
+
+            // check manufacturer id exists
+            RuleFor(x => x.ManufacturerId)
+                .Must(manufacturerId => _context.Manufacturers.Any(c => c.Id == manufacturerId))
+                .WithMessage("The manufacturer does not exist.");
+
+            // check product id exists
+            RuleFor(x => x.ProductId)
+                .Must(productId => _context.Products.Any(c => c.Id == productId))
+                .WithMessage("The product does not exist.");
+        }
+    }
+}
---
+++ Validations/ProductManufacturer/ProductManufacturerMappingCreateValidator.cs
@@ -0,0 +1,13 @@
+﻿using nopCommerceApi.Config;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models.ProductManufacturer;
+
+namespace nopCommerceApi.Validations.ProductManufacturer
+{
+    public class ProductManufacturerMappingCreateValidator : ProductManufacturerMappingBaseValidator<ProductManufacturerMappingCreateDto>
+    {
+        public ProductManufacturerMappingCreateValidator(NopCommerceContext context) : base(context)
+        {
+        }
+    }
+}
