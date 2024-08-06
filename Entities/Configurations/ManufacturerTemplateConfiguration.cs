b474a7a2697581ca6ccec00dd84d6c27cc46aaa1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-06 14:55:22 +0200

    Make manufacturer template entity usable

---
+++ Entities/Configurations/ManufacturerTemplateConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using nopCommerceApi.Entities.Usable;
+using System.Reflection.Emit;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class ManufacturerTemplateConfiguration : IEntityTypeConfiguration<ManufacturerTemplate>
+    {
+        public void Configure(EntityTypeBuilder<ManufacturerTemplate> entity)
+        {
+            entity.ToTable("ManufacturerTemplate");
+
+            entity.Property(e => e.Name).HasMaxLength(400);
+            entity.Property(e => e.ViewPath).HasMaxLength(400);
+        }
+    }
+}
