e485929a4194219aa00a7e13433cb58c128300cf
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-07 21:32:43 +0200

    Create Picture update and create DTO

---
+++ Models/Picture/PictureUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace nopCommerceApi.Models.Picture
+{
+    public class PictureUpdateDto : PictureDto
+    {
+        [JsonIgnore]
+        public override int Id { get; set; }
+
+    }
+}
