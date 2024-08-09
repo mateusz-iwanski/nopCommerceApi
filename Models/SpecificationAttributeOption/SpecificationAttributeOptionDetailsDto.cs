c837ec21b2777264a59fb3c6c43681f8abed0d53
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-09 12:24:19 +0200

    Create SpecificationAttributeOptionDetailsDto

---
+++ Models/SpecificationAttributeOption/SpecificationAttributeOptionDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿using nopCommerceApi.Models.SpecificationAttribute;
+using nopCommerceApi.Models.SpecyficationAttribute;
+
+namespace nopCommerceApi.Models.SpecificationAttributeOption
+{
+    public class SpecificationAttributeOptionDetailsDto : SpecificationAttributeOptionDto
+    {
+        public virtual SpecificationAttributeDto? SpecificationAttribute { get; set;  }
+    }
+}
