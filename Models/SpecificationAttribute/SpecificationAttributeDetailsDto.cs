471790c701119dca88c3eb33b5bf4dfdc2afb57b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-09 12:23:49 +0200

    Create SpecificationAttributeDetailsDto

---
+++ Models/SpecificationAttribute/SpecificationAttributeDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿using nopCommerceApi.Models.SpecyficationAttribute;
+using nopCommerceApi.Models.SpecyficationAttributeGroup;
+
+namespace nopCommerceApi.Models.SpecificationAttribute
+{
+    public class SpecificationAttributeDetailsDto : SpecificationAttributeDto
+    {
+        public virtual SpecificationAttributeGroupDto? SpecificationAttributeGroup { get; set; }
+        public virtual List<SpecificationAttributeOptionDto>? SpecificationAttributeOption { get; set; }
+    }
+}
