56f1db7473b8c766b2e1bcac8e86801e6f07ee78
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-14 15:51:36 +0200

    Rename the directory with validators for ProductManufacturer

---
+++ Validations/ProductManufacturerMaping/ProductManufacturerMappingCreateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using nopCommerceApi.Config;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models.ProductManufacturer;
+
+namespace nopCommerceApi.Validations.ProductManufacturer
+{
+    public class ProductManufacturerMappingCreateValidator : ProductManufacturerMappingBaseValidator<ProductManufacturerMappingCreateDto>
+    {
+        public ProductManufacturerMappingCreateValidator(NopCommerceContext context) : base(context)
+        {
+            // manufacturer id and product id should be just one
+            RuleFor(x => new { x.ManufacturerId, x.ProductId })
+                .Must(x => !_context.ProductManufacturerMappings
+                .Any(pm => pm.ProductId == x.ProductId && pm.ManufacturerId == x.ManufacturerId))
+                .WithMessage("ProductManufacturerMapping exists with product id and manufacturer id");
+        }
+    }
+}
