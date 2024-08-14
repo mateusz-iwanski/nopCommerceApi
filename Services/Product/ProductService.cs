7768240d6246673c143a8dc5ed7087fb912246ed
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-14 16:11:48 +0200

    Async product service

--- Services/Product/ProductService.cs
+++ Services/Product/ProductService.cs
@@ -12,41 +12,41 @@ namespace nopCommerceApi.Services.Product
 {
     public interface IProductService
     {
-        IEnumerable<ProductDto> GetAll();
-        ProductDto GetById(int id);
-        Entities.Usable.Product CreateMinimal(ProductCreateMinimalDto productDto);
-        bool Delete(int id);
-        bool UpdateBlockInformation(int id, ProductUpdateBlockInformationDto productDto);
-        bool UpdateBlockSeo(int id, ProductUpdateBlockSeoDto productDto);
-        bool UpdateBlockRating(int id, ProductUpdateBlockRatingDto productDto);
-        bool UpdateBlockReviews(int id, ProductUpdateBlockReviewsDto productDto);
-        bool UpdateBlockGiftCard(int id, ProductUpdateBlockGiftCardDto productDto);
-        bool UpdateBlockDownload(int id, ProductUpdateBlockDownloadDto productDto);
-        bool UpdateBlockRecurring(int id, ProductUpdateBlockRecurringDto productDto);
-        bool UpdateBlockRentalPrice(int id, ProductUpdateBlockRentalPriceDto productDto);
-        bool UpdateBlockShipping(int id, ProductUpdateBlockShippingDto productDto);
-        bool UpdateBlockInventory(int id, ProductUpdateBlockInventoryDto productDto);
-        bool UpdateBlockAttribute(int id, ProductUpdateBlockAttributeDto productDto);
-        bool UpdateBlockPrice(int id, ProductUpdateBlockPriceDto productDto);
-        bool UnAssociateCategory(int productId, int categoryId);
-        bool AssociateCategory(int productId, int categoryId);
+        Task<IEnumerable<ProductDto>> GetAll();
+        Task<ProductDto> GetById(int id);
+        Task<Entities.Usable.Product> CreateMinimal(ProductCreateMinimalDto productDto);
+        Task<bool> Delete(int id);
+        Task<bool> UpdateBlockInformation(int id, ProductUpdateBlockInformationDto productDto);
+        Task<bool> UpdateBlockSeo(int id, ProductUpdateBlockSeoDto productDto);
+        Task<bool> UpdateBlockRating(int id, ProductUpdateBlockRatingDto productDto);
+        Task<bool> UpdateBlockReviews(int id, ProductUpdateBlockReviewsDto productDto);
+        Task<bool> UpdateBlockGiftCard(int id, ProductUpdateBlockGiftCardDto productDto);
+        Task<bool> UpdateBlockDownload(int id, ProductUpdateBlockDownloadDto productDto);
+        Task<bool> UpdateBlockRecurring(int id, ProductUpdateBlockRecurringDto productDto);
+        Task<bool> UpdateBlockRentalPrice(int id, ProductUpdateBlockRentalPriceDto productDto);
+        Task<bool> UpdateBlockShipping(int id, ProductUpdateBlockShippingDto productDto);
+        Task<bool> UpdateBlockInventory(int id, ProductUpdateBlockInventoryDto productDto);
+        Task<bool> UpdateBlockAttribute(int id, ProductUpdateBlockAttributeDto productDto);
+        Task<bool> UpdateBlockPrice(int id, ProductUpdateBlockPriceDto productDto);
+        Task<bool> UnAssociateCategory(int productId, int categoryId);
+        Task<bool> AssociateCategory(int productId, int categoryId);
     }
 
     public class ProductService : BaseService, IProductService
     {
-        public ProductService(NopCommerceContext context, IMapper mapper, ILogger<ProductService> logger) : base (context, mapper, logger) { }
+        public ProductService(NopCommerceContext context, IMapper mapper, ILogger<ProductService> logger) : base(context, mapper, logger) { }
 
-        public IEnumerable<ProductDto> GetAll()
+        public async Task<IEnumerable<ProductDto>> GetAll()
         {
-            var products = _context.Products.ToList();
+            var products = await _context.Products.ToListAsync();
             var productDtos = _mapper.Map<List<ProductDto>>(products);
 
             return productDtos;
         }
 
-        public ProductDto GetById(int id)
+        public async Task<ProductDto> GetById(int id)
         {
-            var products = _context.Products.FirstOrDefault(p => p.Id == id);
+            var products = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (products == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
@@ -55,194 +55,195 @@ namespace nopCommerceApi.Services.Product
             return productDto;
         }
 
-        public Entities.Usable.Product CreateMinimal(ProductCreateMinimalDto productDto)
+        public async Task<Entities.Usable.Product> CreateMinimal(ProductCreateMinimalDto productDto)
         {
             var product = _mapper.Map<Entities.Usable.Product>(productDto);
 
             _context.Products.Add(product);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return product;
         }
 
-        public bool Delete(int id)
+        public async Task<bool> Delete(int id)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockInformation(int id, ProductUpdateBlockInformationDto productDto)
+        public async Task<bool> UpdateBlockInformation(int id, ProductUpdateBlockInformationDto productDto)
         {
             productDto.Id = id;
 
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockSeo(int id, ProductUpdateBlockSeoDto productDto)
+        public async Task<bool> UpdateBlockSeo(int id, ProductUpdateBlockSeoDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockRating(int id, ProductUpdateBlockRatingDto productDto)
+        public async Task<bool> UpdateBlockRating(int id, ProductUpdateBlockRatingDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockReviews(int id, ProductUpdateBlockReviewsDto productDto)
+        public async Task<bool> UpdateBlockReviews(int id, ProductUpdateBlockReviewsDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockGiftCard(int id, ProductUpdateBlockGiftCardDto productDto)
+        public async Task<bool> UpdateBlockGiftCard(int id, ProductUpdateBlockGiftCardDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockDownload(int id, ProductUpdateBlockDownloadDto productDto)
+        public async Task<bool> UpdateBlockDownload(int id, ProductUpdateBlockDownloadDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockRecurring(int id, ProductUpdateBlockRecurringDto productDto)
+        public async Task<bool> UpdateBlockRecurring(int id, ProductUpdateBlockRecurringDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockRentalPrice(int id, ProductUpdateBlockRentalPriceDto productDto)
+        public async Task<bool> UpdateBlockRentalPrice(int id, ProductUpdateBlockRentalPriceDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockShipping(int id, ProductUpdateBlockShippingDto productDto)
+        public async Task<bool> UpdateBlockShipping(int id, ProductUpdateBlockShippingDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockInventory(int id, ProductUpdateBlockInventoryDto productDto)
+        public async Task<bool> UpdateBlockInventory(int id, ProductUpdateBlockInventoryDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockAttribute(int id, ProductUpdateBlockAttributeDto productDto)
+        public async Task<bool> UpdateBlockAttribute(int id, ProductUpdateBlockAttributeDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UpdateBlockPrice(int id, ProductUpdateBlockPriceDto productDto)
+        public async Task<bool> UpdateBlockPrice(int id, ProductUpdateBlockPriceDto productDto)
         {
-            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {id} not found");
 
             _mapper.Map(productDto, product);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool AssociateCategory(int productId, int categoryId)
+        public async Task<bool> AssociateCategory(int productId, int categoryId)
         {
-            var product = _context.Products
+            var product = await _context.Products
                 .Include(p => p.ProductCategoryMappings)
-                .FirstOrDefault(p => p.Id == productId);
+                .FirstOrDefaultAsync(p => p.Id == productId);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {productId} not found");
 
-            var category = _context.Categories.FirstOrDefault(c => c.Id == categoryId);
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == categoryId);
 
             if (category == null) throw new NotFoundExceptions($"Category with id {categoryId} not found");
 
@@ -254,20 +255,20 @@ namespace nopCommerceApi.Services.Product
 
             product.ProductCategoryMappings.Add(new ProductCategoryMapping { ProductId = productId, CategoryId = categoryId });
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool UnAssociateCategory(int productId, int categoryId)
+        public async Task<bool> UnAssociateCategory(int productId, int categoryId)
         {
-            var product = _context.Products
+            var product = await _context.Products
                 .Include(p => p.ProductCategoryMappings)
-                .FirstOrDefault(p => p.Id == productId);
+                .FirstOrDefaultAsync(p => p.Id == productId);
 
             if (product == null) throw new NotFoundExceptions($"Product with id {productId} not found");
 
-            var category = _context.Categories.FirstOrDefault(c => c.Id == categoryId);
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == categoryId);
 
             if (category == null) throw new NotFoundExceptions($"Category with id {categoryId} not found");
 
@@ -281,12 +282,9 @@ namespace nopCommerceApi.Services.Product
 
             product.ProductCategoryMappings.Remove(productCategoryMapping);
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
-
-
-
     }
 }
