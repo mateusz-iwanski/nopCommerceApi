8bd98cc2a2601cf4b62a81185602da8d9654732f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-14 14:29:45 +0200

    Add summary for ProductAttributeValueUpdateDto

--- Models/ProductAttributeValue/ProductAttributeValueUpdateDto.cs
+++ Models/ProductAttributeValue/ProductAttributeValueUpdateDto.cs
@@ -1,10 +1,17 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace nopCommerceApi.Models.ProductAttributeValue
 {
     public class ProductAttributeValueUpdateDto : ProductAttributeValueDto
     {
+        /// <summary>
+        /// ## ProductAttributeMappingId
+        /// ### Gets or sets the product attribute mapping identifier
+        /// #### Gets or sets product id which is associate with this attribute value
+        /// #### It's id of product
+        /// </summary>
         [JsonIgnore]
-        public override int Id { get; set; }
+        public virtual int ProductAttributeMappingId { get; set; }
     }
 }
