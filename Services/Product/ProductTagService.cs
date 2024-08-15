089d2f300dd48093ba4011768f4660e64fdb45f1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-15 15:08:12 +0200

    Async and disable tracking for get actions in ProductTagService

--- Services/Product/ProductTagService.cs
+++ Services/Product/ProductTagService.cs
@@ -1,21 +1,22 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using nopCommerceApi.Entities;
 using nopCommerceApi.Entities.Usable;
 using nopCommerceApi.Exceptions;
-using nopCommerceApi.Models.Product;
+using nopCommerceApi.Models.ProductTag;
 
 namespace nopCommerceApi.Services.Product
 {
     public interface IProductTagService
     {
-        ProductTag Create(ProductTagCreateDto productTagDto);
-        bool Delete(int id);
-        IEnumerable<ProductTagDto> GetAll();
-        IEnumerable<ProductTagDetailsDto> GetAllDteils();
-        ProductTagDto GetById(int id);
-        IEnumerable<ProductTagDto> GetByTag(string tagName);
-        bool Update(int id, [FromBody] ProductTagUpdateDto productTagDto);
+        Task<ProductTag> CreateAsync(ProductTagCreateDto productTagDto);
+        Task<bool> DeleteAsync(int id);
+        Task<IEnumerable<ProductTagDto>> GetAllAsync();
+        Task<IEnumerable<ProductTagDetailsDto>> GetAllDetailsAsync();
+        Task<ProductTagDto> GetByIdAsync(int id);
+        Task<IEnumerable<ProductTagDto>> GetByTagAsync(string tagName);
+        Task<bool> UpdateAsync([FromBody] ProductTagUpdateDto productTagDto);
     }
 
     public class ProductTagService : BaseService, IProductTagService
@@ -23,70 +24,79 @@ namespace nopCommerceApi.Services.Product
         public ProductTagService(NopCommerceContext context, IMapper mapper, ILogger<ProductTagService> logger)
             : base(context, mapper, logger) { }
 
-        public IEnumerable<ProductTagDto> GetAll()
+        public async Task<IEnumerable<ProductTagDto>> GetAllAsync()
         {
-            var productTags = _context.ProductTags.ToList();
+            var productTags = await _context.ProductTags
+                .AsNoTracking()
+                .ToListAsync();
+
             var productTagDtos = _mapper.Map<List<ProductTagDto>>(productTags);
 
             return productTagDtos;
         }
 
-        public IEnumerable<ProductTagDetailsDto> GetAllDteils()
+        public async Task<IEnumerable<ProductTagDetailsDto>> GetAllDetailsAsync()
         {
-            var productTags = _context.ProductTags.ToList();
+            var productTags = await _context.ProductTags
+                .AsNoTracking()
+                .ToListAsync();
+
             var productTagDtos = _mapper.Map<List<ProductTagDetailsDto>>(productTags);
 
             return productTagDtos;
         }
 
-        public IEnumerable<ProductTagDto> GetByTag(string tagName)
+        public async Task<IEnumerable<ProductTagDto>> GetByTagAsync(string tagName)
         {
-            var productTags = _context.ProductTags.Where(p => p.Name.Contains(tagName)).ToList();
+            var productTags = await _context.ProductTags
+                .AsNoTracking()
+                .Where(p => p.Name.Contains(tagName)).ToListAsync();
+
             var productTagDtos = _mapper.Map<List<ProductTagDto>>(productTags);
 
             return productTagDtos;
         }
 
-        public ProductTagDto GetById(int id)
+        public async Task<ProductTagDto> GetByIdAsync(int id)
         {
-            var productTag = _context.ProductTags.FirstOrDefault(p => p.Id == id);
+            var productTag = await _context.ProductTags
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
 
             var productTagDto = productTag != null ? _mapper.Map<ProductTagDto>(productTag) : throw new NotFoundExceptions($"ProductTag with id {id} not found");
 
             return productTagDto;
         }
 
-        public ProductTag Create(ProductTagCreateDto productTagDto)
+        public async Task<ProductTag> CreateAsync(ProductTagCreateDto productTagDto)
         {
             var productTag = _mapper.Map<ProductTag>(productTagDto);
 
             _context.ProductTags.Add(productTag);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return productTag;
         }
 
-        public bool Update(int id, [FromBody] ProductTagUpdateDto productTagDto)
+        public async Task<bool> UpdateAsync([FromBody] ProductTagUpdateDto productTagDto)
         {
-            var productTag = _context.ProductTags.FirstOrDefault(p => p.Id == id);
-
-            if (productTag == null) throw new NotFoundExceptions($"Product Tag with {id} not found.");
+            var productTag = await _context.ProductTags.FirstOrDefaultAsync(p => p.Id == productTagDto.Id);
 
             productTag.Name = productTagDto.Name;
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
 
-        public bool Delete(int id)
+        public async Task<bool> DeleteAsync(int id)
         {
-            var productTag = _context.ProductTags.FirstOrDefault(p => p.Id == id);
+            var productTag = await _context.ProductTags.FirstOrDefaultAsync(p => p.Id == id);
 
             if (productTag == null) throw new NotFoundExceptions($"Product Tag with {id} not found.");
 
             _context.ProductTags.Remove(productTag);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return true;
         }
