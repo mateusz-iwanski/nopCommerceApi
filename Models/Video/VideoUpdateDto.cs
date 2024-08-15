f359dca5ba13b4b36db78918192eda7cfde50cc3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-15 17:20:53 +0200

    Make id required in video update dto

--- Models/Video/VideoUpdateDto.cs
+++ Models/Video/VideoUpdateDto.cs
@@ -1,6 +1,10 @@
-﻿namespace nopCommerceApi.Models.Video
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nopCommerceApi.Models.Video
 {
     public class VideoUpdateDto : VideoDto
     {
+        [Required]
+        public override int Id { get; set; }
     }
 }
