6f50821a660e9cb72ce1c8da43bc3bf13c5cc8a2
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-15 14:44:34 +0200

    Create rule for checking if product id and specification attribute option id combination exists, exlude the updated object in ProductSpecificationAttributeMappingUpdateDtoValidator

--- Validations/ProductSpecificationAttributeMapping/ProductSpecificationAttributeMappingUpdateDtoValidator.cs
+++ Validations/ProductSpecificationAttributeMapping/ProductSpecificationAttributeMappingUpdateDtoValidator.cs
@@ -1,4 +1,5 @@
-﻿using nopCommerceApi.Entities;
+﻿using FluentValidation;
+using nopCommerceApi.Entities;
 using nopCommerceApi.Models.ProductSpecificationAttributeMapping;
 
 namespace nopCommerceApi.Validations.ProductSpecificationAttributeMapping
@@ -7,6 +8,17 @@ namespace nopCommerceApi.Validations.ProductSpecificationAttributeMapping
     {
         public ProductSpecificationAttributeMappingUpdateDtoValidator(NopCommerceContext context) : base(context)
         {
+            // check id exists
+            RuleFor(x => x.Id)
+                .Must(x => _context.ProductSpecificationAttributeMappings.Find(x) != null)
+                .WithMessage("Product specification attribute mapping id does not exist");
+
+            // check if product id and specification attribute option id combination exists, exclude the current mapping
+            RuleFor(x => new { x.ProductId, x.SpecificationAttributeOptionId, x.Id})
+                .Must(x => _context.ProductSpecificationAttributeMappings.FirstOrDefault(
+                    psam => x.ProductId == psam.ProductId && x.SpecificationAttributeOptionId == psam.SpecificationAttributeOptionId && x.Id != psam.Id) == null
+                    )
+                .WithMessage("Product id and specification attribute option id combination already exists");
         }
     }
 }
