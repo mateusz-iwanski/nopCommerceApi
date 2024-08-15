fd13f5c5f71703b2543ee0c651986ad4ccdbd4b7
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-15 14:15:28 +0200

    Remove rule from ProductUpdateBlockAttributeDto validator

--- Validations/Product/ProductUpdateBlockAttributeDtoValidator.cs
+++ Validations/Product/ProductUpdateBlockAttributeDtoValidator.cs
@@ -1,5 +1,4 @@
-﻿using FluentValidation;
-using nopCommerceApi.Models.Product;
+﻿using nopCommerceApi.Models.Product;
 
 namespace nopCommerceApi.Validations
 {
@@ -7,9 +6,6 @@ namespace nopCommerceApi.Validations
     {
         public ProductUpdateBlockAttributeDtoValidator() : base()
         {
-            // check product exists
-            RuleFor(x => x.Id)
-                .Must(id => id > 0)
         }
     }
 }
