ab2c87a90a0d282789aea6d407446ad34d8d95a0
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-15 14:44:00 +0200

    Create rule for checking if product id and specification attribute option id combination exists in ProductSpecificationAttributeMappingCreateDtoValidator

--- Validations/ProductSpecificationAttributeMapping/ProductSpecificationAttributeMappingCreateDtoValidator.cs
+++ Validations/ProductSpecificationAttributeMapping/ProductSpecificationAttributeMappingCreateDtoValidator.cs
@@ -1,4 +1,5 @@
-﻿using nopCommerceApi.Entities;
+﻿using FluentValidation;
+using nopCommerceApi.Entities;
 using nopCommerceApi.Models.ProductSpecificationAttributeMapping;
 
 namespace nopCommerceApi.Validations.ProductSpecificationAttributeMapping
@@ -6,7 +7,13 @@ namespace nopCommerceApi.Validations.ProductSpecificationAttributeMapping
     public class ProductSpecificationAttributeMappingCreateDtoValidator : ProductSpecificationAttributeMappingBaseDtoValidator<ProductSpecificationAttributeMappingCreateDto>
     {
         public ProductSpecificationAttributeMappingCreateDtoValidator(NopCommerceContext context) : base(context)
-        { 
+        {
+            // check if product id and specification attribute option id combination exists
+            RuleFor(x => new { x.ProductId, x.SpecificationAttributeOptionId, x.Id })
+                .Must(x => _context.ProductSpecificationAttributeMappings.FirstOrDefault(
+                    psam => x.ProductId == psam.ProductId && x.SpecificationAttributeOptionId == psam.SpecificationAttributeOptionId) == null
+                    )
+                .WithMessage("Product id and specification attribute option id combination already exists");
         }
     }
 }
