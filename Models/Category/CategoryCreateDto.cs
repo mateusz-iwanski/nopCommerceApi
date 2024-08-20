e1779d6e8af1a1b0b10b4524bee2129d4c5da1e2
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-20 21:10:06 +0200

    Change getter and setter for  CreatedOnUtc and UpdatedOnUtc in CategoryCreateDto

--- Models/Category/CategoryCreateDto.cs
+++ Models/Category/CategoryCreateDto.cs
@@ -4,14 +4,25 @@ namespace nopCommerceApi.Models.Category
 {
     public class CategoryCreateDto : CategoryDto
     {
+        private DateTime _createdOnUtc { get; set; }
+        private DateTime _uptadeOnUtc { get; set; }
+
         [JsonIgnore]
         public override int Id { get; set; }
 
         [JsonIgnore]
-        public override DateTime CreatedOnUtc { get; set; } = DateTime.Now;
+        public override DateTime CreatedOnUtc 
+        { 
+            get => _createdOnUtc;
+            set => _createdOnUtc = DateTime.Now;
+        } 
 
         [JsonIgnore]
-        public override DateTime UpdatedOnUtc { get; set; } = DateTime.Now;
+        public override DateTime UpdatedOnUtc
+        {
+            get => _uptadeOnUtc;
+            set => _uptadeOnUtc = DateTime.Now;
+        }
 
         // if we create a new category, it should be deleted = false
         [JsonIgnore]
