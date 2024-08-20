840f4eeda52cb72e6db3f4177a30b9d6c93032b3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-20 21:10:45 +0200

    Change the getter and setter for UpdatedOnUtc in CategoryUpdateDto

--- Models/Category/CategoryUpdateDto.cs
+++ Models/Category/CategoryUpdateDto.cs
@@ -4,7 +4,13 @@ namespace nopCommerceApi.Models.Category
 {
     public class CategoryUpdateDto : CategoryDto
     {
+        private DateTime _uptadeOnUtc { get; set; }
+
         [JsonIgnore]
-        public override DateTime UpdatedOnUtc { get; set; } = DateTime.Now;
+        public override DateTime UpdatedOnUtc
+        {
+            get => _uptadeOnUtc;
+            set => _uptadeOnUtc = DateTime.Now;
+        }
     }
 }
