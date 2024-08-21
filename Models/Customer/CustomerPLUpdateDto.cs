f5af3cab59e0d3fb0e6c5a45c281858668219224
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 15:34:17 +0200

    Create CustomerPLUpdateDto

---
+++ Models/Customer/CustomerPLUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace nopCommerceApi.Models.Customer
+{
+    public class CustomerPLUpdateDto : CustomerPLCreateBaseDto
+    {
+        [Required]
+        public int Id { get; set; }        
+
+        public override DateTime CreatedOnUtc { get; set; }
+
+        [JsonIgnore]
+        public override string? Password { get; set; }
+    }
+}
