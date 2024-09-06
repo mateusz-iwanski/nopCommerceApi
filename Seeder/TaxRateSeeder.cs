83d9307bd65652ca08164f81df881a423c670ec3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-06 18:07:43 +0200

    Change the name of the default tax category

--- Seeder/TaxRateSeeder.cs
+++ Seeder/TaxRateSeeder.cs
@@ -25,8 +25,8 @@ namespace nopCommerceApi.Seeder
             var country = _context.Countries.FirstOrDefault(c => c.Name == "Poland");
             var countryId = country?.Id ?? throw new BadRequestException("Country \"Poland\" not exist. Add to database.");
 
-            var taxCategory23 = _context.TaxCategories.FirstOrDefault(tc => tc.Name == "PL-23% (Podstawowa)");
-            var taxCategory23Id = taxCategory23?.Id ?? throw new BadRequestException("Default tax category \"PL-23% (Podstawowa) not exists\", use TaxCategorySeeder.");
+            var taxCategory23 = _context.TaxCategories.FirstOrDefault(tc => tc.Name == "PL_23_procent_podstawowa");
+            var taxCategory23Id = taxCategory23?.Id ?? throw new BadRequestException("Default tax category \"PL_23_procent_podstawowa\" not exists, use TaxCategorySeeder.");
             if (!_context.TaxRates.Any(tr => tr.TaxCategoryId == taxCategory23Id && tr.CountryId == countryId))
             {
                 var taxRate23 = new TaxRate
@@ -42,8 +42,8 @@ namespace nopCommerceApi.Seeder
             }
 
 
-            var taxCategory8 = _context.TaxCategories.FirstOrDefault(tc => tc.Name == "PL-8% (Podstawowa)");
-            var taxCategory8Id = taxCategory8?.Id ?? throw new BadRequestException("Default tax category \"PL-8% (Podstawowa) not exists\", use TaxCategorySeeder.");
+            var taxCategory8 = _context.TaxCategories.FirstOrDefault(tc => tc.Name == "PL_8_procent_podstawowa");
+            var taxCategory8Id = taxCategory8?.Id ?? throw new BadRequestException("Default tax category \"PL_8_procent_podstawowa\" not exists, use TaxCategorySeeder.");
             if (!_context.TaxRates.Any(tr => tr.TaxCategoryId == taxCategory8Id && tr.CountryId == countryId))
             {
                 var taxRate23 = new TaxRate
