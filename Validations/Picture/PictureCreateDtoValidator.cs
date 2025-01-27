f5d90dd809ac3ac4ebd48d6375a6413007e0420c
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-27 16:27:01 +0100

    Disable uniqueness validation for SeoFilename
    The validation rule that checks if the `SeoFilename` of a picture
    is unique in the database has been commented out. This means that
    the `PictureCreateDtoValidator` will no longer enforce the
    uniqueness of the `SeoFilename` property.

--- Validations/Picture/PictureCreateDtoValidator.cs
+++ Validations/Picture/PictureCreateDtoValidator.cs
@@ -10,9 +10,9 @@ namespace nopCommerceApi.Validations.Picture
         public PictureCreateDtoValidator(NopCommerceContext context, IMySettings settings) : base(context, settings)
         {
             // check picture name is unique
-            RuleFor(x => x.SeoFilename)
-                .Must(name => context.Pictures.Any(p => p.SeoFilename == name) == false)
-                .WithMessage("Picture SEO file name already exists in the database. Must be unique.");
+            //RuleFor(x => x.SeoFilename)
+            //    .Must(name => context.Pictures.Any(p => p.SeoFilename == name) == false)
+            //    .WithMessage("Picture SEO file name already exists in the database. Must be unique.");
         }
     }
 }
