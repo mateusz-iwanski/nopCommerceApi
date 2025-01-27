6c7eaa1828e1e3b0b18f0f28b4fcf6660414e1f8
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-27 16:24:35 +0100

    Add PictureBinaryCreateDto with JsonIgnore on Id
    Introduce PictureBinaryCreateDto in nopCommerceApi.Models.Picture.
    Inherit from PictureBinaryDto and override Id property with
    [JsonIgnore] to exclude it from JSON serialization. Add using
    directive for System.Text.Json.Serialization.

---
+++ Models/Picture/PictureBinaryCreateDto.cs.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace nopCommerceApi.Models.Picture
+{
+    public class PictureBinaryCreateDto : PictureBinaryDto
+    {
+        [JsonIgnore]
+        public override int Id { get; set; }
+    }
+}
