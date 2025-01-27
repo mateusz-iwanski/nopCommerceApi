45a5f74826124d80dfd19050755fcec500402a94
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-27 16:26:50 +0100

    Add PictureBinaryCreateDtoValidator for picture validation
    Introduce PictureBinaryCreateDtoValidator in nopCommerceApi.Validations.Picture.
    Inherit from BaseValidator<PictureBinaryCreateDto>.
    Add constructor with NopCommerceContext and IMySettings parameters.
    Add validation rule for PictureId to check existence in Pictures table.
    Include necessary using directives: FluentValidation, Microsoft.EntityFrameworkCore, nopCommerceApi.Config, nopCommerceApi.Entities, nopCommerceApi.Models.Picture.

---
+++ Validations/Picture/PictureBinaryCreateDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using nopCommerceApi.Config;
+using nopCommerceApi.Entities;
+using nopCommerceApi.Models.Picture;
+
+namespace nopCommerceApi.Validations.Picture
+{
+    public class PictureBinaryCreateDtoValidator : BaseValidator<PictureBinaryCreateDto>
+    {
+        public PictureBinaryCreateDtoValidator(NopCommerceContext context, IMySettings settings)
+        {
+
+            RuleFor(x => x.PictureId)
+                .Must(pictureId => context.Pictures.Find(pictureId) != null)
+                .WithMessage("Picture does not exist");
+        }
+    }
+}
