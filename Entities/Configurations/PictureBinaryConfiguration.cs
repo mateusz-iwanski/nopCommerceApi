a300dd9d8154455c243bc3c9de452a6ba869cbde
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-27 16:23:59 +0100

    Add PictureBinaryConfiguration class for EF Core setup
    Introduced a new class `PictureBinaryConfiguration` in the `nopCommerceApi.Entities.Configurations` namespace. This class implements the `IEntityTypeConfiguration<PictureBinary>` interface to configure the `PictureBinary` entity.
    
    - Configured the table name to "PictureBinary".
    - Set the primary key and named it "PK_PictureBinary".
    - Configured the `Id` property to be auto-generated.
    - Made the `PictureId` property required.
    - Set the `BinaryData` property type to `varbinary(max)`.
    - Created a unique index on the `Id` property.

---
+++ Entities/Configurations/PictureBinaryConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using nopCommerceApi.Entities.Usable;
+
+namespace nopCommerceApi.Entities.Configurations
+{
+    public class PictureBinaryConfiguration : IEntityTypeConfiguration<PictureBinary>
+    {
+        public void Configure(EntityTypeBuilder<PictureBinary> builder)
+        {
+            builder.ToTable("PictureBinary");
+
+            builder.HasKey(pb => pb.Id)
+                   .HasName("PK_PictureBinary");
+
+            builder.Property(pb => pb.Id)
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(pb => pb.PictureId)
+                   .IsRequired();
+
+            builder.Property(pb => pb.BinaryData)
+                   .HasColumnType("varbinary(max)");
+
+            builder.HasIndex(pb => pb.Id)
+                   .IsUnique();
+        }
+    }
+}
