e2c612150ba2543eb1afa8f0323497f0c863470a
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-13 07:54:45 +0100

    Add DeliveryDateCreateDto class with JsonIgnore on Id
    Introduce DeliveryDateCreateDto in nopCommerceApi.Models.DeliveryDate.
    This class extends DeliveryDateDto and overrides the Id property,
    marking it with [JsonIgnore] to exclude it from JSON serialization.
    This is useful for creating new delivery date entries without
    including the Id in the request payload.

---
+++ Models/DelivaeryDate/DeliveryDateCreateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace nopCommerceApi.Models.DelivaeryDate
+{
+    public class DeliveryDateCreateDto : DeliveryDateDto
+    {
+        [JsonIgnore]
+        public override int Id { get; set; }
+    }
+}
