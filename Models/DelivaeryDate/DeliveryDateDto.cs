68b99653fe93fcc3c506dc536fab23e0da81ad31
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-02-13 07:55:01 +0100

    Add DeliveryDateDto class to DelivaeryDate namespace
    A new data transfer object (DTO) class named `DeliveryDateDto` has been added to the `nopCommerceApi.Models.DelivaeryDate` namespace. This class represents delivery date information and includes properties for an identifier (`Id`), a name (`Name`), and a display order (`DisplayOrder`). The class inherits from `BaseDto`, indicating it shares common properties or methods with other DTOs in the application.

---
+++ Models/DelivaeryDate/DeliveryDateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace nopCommerceApi.Models.DelivaeryDate
+{
+    public class DeliveryDateDto : BaseDto
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; } = null!;
+        public virtual int DisplayOrder { get; set; }
+    }
+}
